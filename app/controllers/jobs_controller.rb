52d91b625a08894f64fba66a16e5ddec4afafd8a
xdite <xuite.joke@gmail.com>
2016-07-24 13:55:06 +0800

    edit, update, new, create,destroy need login

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
   def show
     @job = Job.find(params[:id])
   end
