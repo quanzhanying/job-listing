bba8c16b48718f86b613229f6824ebbb49612cb2
xdite <xuite.joke@gmail.com>
2016-07-24 16:41:36 +0800

    Refactor admin? condition to model

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -46,7 +46,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def require_is_admin
-    if current_user.email != 'xdite@growth.school'
+    unless current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    email == 'xdite@growth.school'
+  end
 end
