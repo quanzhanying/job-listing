6f173babb69b1147cd8ba054d4a230d8eb6d3230
chpwang <chpengwang@gmail.com>
2016-07-25 13:50:18 +0800

    install simple_form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
 	before_action :authenticate_admin!, only: [:index]
 
 	def index
-		flash[:warning] = "Need Lunch ..."
+		flash[:notice] = "Lunch Finished..."
 	end
 end
