2e849debeb3c9ce42c2ad393ff335bb3513dba08
xdite <xuite.joke@gmail.com>
2016-07-25 21:53:06 +0800

    disable all user create content

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,40 +9,40 @@ class JobsController < ApplicationController
     @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
-  def new
-    @job = Job.new
-  end
+#  def new
+#    @job = Job.new
+#  end
 
-  def create
-    @job = Job.new(job_params)
+#  def create
+#    @job = Job.new(job_params)
 
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
-  end
+#    if @job.save
+#      redirect_to jobs_path
+#    else
+#      render :new
+#    end
+#  end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
+#  def edit
+#    @job = Job.find(params[:id])
+#  end
 
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
-  end
+#  def update
+#    @job = Job.find(params[:id])
+#    if @job.update(job_params)
+#      redirect_to jobs_path
+#    else
+#      render :edit
+#    end
+#  end
 
-  def destroy
-    @job = Job.find(params[:id])
+#  def destroy
+#    @job = Job.find(params[:id])
 
-    @job.destroy
-
-    redirect_to jobs_path
-  end
+#    @job.destroy
+#
+#    redirect_to jobs_path
+#  end
 
   private
 
