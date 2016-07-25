ef4610de1ea1371f7940d7151735fe82cbc73608
liyanzeng <yanzengli@gmail.com>
2016-07-25 16:31:16 +0800

    add login/logout

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,43 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.all
+  end
+  def show
+     @group = Group.find(params[:id])
+   end
+
+   def edit
+     @group = Group.find(params[:id])
+   end
+
+  def create
+     @group = Group.new(group_params)
+     @group.user = current_user
+    if @group.save
+
+     redirect_to groups_path
+   else
+     render :new
+   end
+   end
+
+   def update
+    @group = Group.find(params[:id])
+
+    @group.update(group_params)
+
+    redirect_to groups_path, notice: "Update Success"
+  end
+
+   private
+
+   def group_params
+     params.require(:group).permit(:title, :description)
+   end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root "welcome#index"
+  devise_for :users
+  resources :groups
+  root "groups#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
