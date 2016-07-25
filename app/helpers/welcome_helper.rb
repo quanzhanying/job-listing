0ab4585ea3d2a2ac88ae7b72ea16b895dd4fe6da
liber17321 <tianwh3721@gmail.com>
2016-07-25 14:32:02 +0800

    add bootstrap flash function

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
+
+  def flash_class(key)
+    FLASH_CLASSES.fetch key.to_sym, keyend
+  end
+
+  def user_facing_flashes
+    flash.to_hash.slice "alert", "notice", "warning"
+  end
 end
