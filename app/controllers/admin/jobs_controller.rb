0c585a58ca5797978986cf84e79952e9c5a543b8
nieyicheng <nieyicheng1990@gmail.com>
2016-07-26 18:08:17 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -43,12 +43,6 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path, alert: "Job deleted"
   end
 
-  def require_is_admin
-     if !current_user.admin?
-       flash[:alert] = 'You are not admin'
-       redirect_to root_path
-     end
-   end
 
   private
 
