9a5f000202b9e0371ffb43b10261482064ddd8cd
liyanzeng <yanzengli@gmail.com>
2016-07-26 14:19:14 +0800

    implement jobs#show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,20 @@ def new
   @job = Job.new
 end
 
+def show
+  @job = Job.find(params[:id])
+end
+
+def create
+  @job = Job.new(job_params)
+  @job.save
+  redirect_to jobs_path
+end
+
+private
+
+def job_params
+  params.require(:job).permit(:title, :description)
+end
+
 end
