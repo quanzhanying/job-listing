dab9af6a4eb744fab2833c728d5e081fcb1422d8
kaihuasun@163.com <kaihuasun@163.com>
2016-07-26 15:51:44 +0800

    implement jobs#update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,10 +11,22 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def create
     @job = Job.new(job_params)
     @job.save
     redirect_to jobs_path
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    @job.update(job_params)
+
+    redirect_to jobs_path, notice: "Upadate Success"
   end
 
 private
