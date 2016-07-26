cc372c54277474a7ec19ff57f41b9cce4c623552
chpwang <chpengwang@gmail.com>
2016-07-26 11:32:45 +0800

    add jobs_controller and index, edit, show, new html files

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,10 @@
+class JobsController < ApplicationController
+
+	def index
+		
+	end
+
+	def new
+		
+	end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
-	before_action :authenticate_admin!, only: [:index]
+	
 
 	def index
 		flash[:notice] = "Lunch Finished..."
