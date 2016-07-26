419f24217e2e5d583cd4050b4efbcd10717e4fb6
chenyl <18301662790@163.com>
2016-07-26 12:36:59 +0800

    implement CRUD for job_controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,51 @@
 class JobsController < ApplicationController
+    before_action :find_job_and_check_permission,only:[:edit,:update,:destroy]
+
     def index
       @jobs = Job.all
+    end
+
+    def show
+      @job = Job.find(params[:id])
+    end
+
+    def new
+      @job = Job.new
+    end
+
+    def create
+      @job = Job.new(job_params)
+
+      if @job.save
+        redirect_to jobs_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @job.update(job_params)
+        redirect_to jobs_path,notice: "Update Success"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @job.destroy
+      redirect_to jobs_path,alert: "Job Deleted"
+    end
+
+    private
+
+    def job_params
+      params.require(:job).permit(:title,:description)
+    end
+
+    def find_job_and_check_permission
+      @job = Job.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  'welcome#index'
+  #root  'welcome#index'
+  root 'jobs#index'
 end
