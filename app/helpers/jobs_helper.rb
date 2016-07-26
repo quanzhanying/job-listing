9483179841cf9d8c1d2047e388cedcdfde432973
shaojunda <shaojunda@gmail.com>
2016-07-27 01:10:49 +0800

    user render_job_status replace ife; implements step4

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,4 +2,12 @@ module JobsHelper
   def render_job_description(job)
     simple_format(job.description)
   end
+
+  def render_job_status(job)
+    if job.is_hidden
+      "( Private )"
+    else
+      "( Public )"
+    end
+  end
 end
