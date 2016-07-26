cfd442738caab43fb73d3bd57bfbb38af615ddbf
liyanzeng <yanzengli@gmail.com>
2016-07-26 14:27:25 +0800

    implement job#update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,11 +11,22 @@ def show
   @job = Job.find(params[:id])
 end
 
+def edit
+  @job = Job.find(params[:id])
+end
+
 def create
   @job = Job.new(job_params)
   @job.save
   redirect_to jobs_path
 end
+
+def update
+  @job = Job.find(params[:id])
+  @job.update(job_params)
+  redirect_to jobs_path, notice: "Update Success"
+end
+
 
 private
 
