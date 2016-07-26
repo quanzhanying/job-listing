d04db3559562341a3fb1278fc89b6475f4b6e354
linzhewei <zp8106@gmail.com>
2016-07-26 17:50:10 +0800

    implement part2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,4 +1,6 @@
 class Admin::JobsController < ApplicationController
+  
+
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_filter :require_is_admin
   def index
@@ -49,10 +51,5 @@ class Admin::JobsController < ApplicationController
     params.require(:job).permit(:title, :description)
   end
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-    email == '123@123.123'
+    is_admin
   end
 end
