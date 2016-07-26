74a1c864aaf2f35be86ed19443d57a40c50fb2bc
Jane Du <Jane.dujuan.hr@gmail.com>
2016-07-26 17:50:37 +0800

    add create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,26 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job=Job.new
+  end
+
+  def create
+    @group = Job.new(job_params)
+    @group.save
+    redirect_to jobs_path
+  end
+
+  def edit
+
+
+  end
+
+private
+
+def job_params
+  params.require(:job).permit(:title,:description)
+end
+
 end
