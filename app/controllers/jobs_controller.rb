12bc4eeff1c2c6c63028b47d95017c1c3efea3a6
kaihuasun@163.com <kaihuasun@163.com>
2016-07-26 15:33:22 +0800

    implement jobs#create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,16 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
+
+  def create
+    @job = Job.new(job_params)
+    @job.save
+    redirect_to jobs_path
+  end
+
+private
+
+def job_params
+  params.require(:job).permit(:title, :description)
 end
+  end
