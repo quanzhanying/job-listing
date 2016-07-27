2e0a1b84642a7f4ca8ec20ced524d654078b25f1
kaihuasun@163.com <kaihuasun@163.com>
2016-07-27 13:06:42 +0800

    complete step3 wage_bound set

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  description      :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  wage_upper_bound :integer
+#  wage_lower_bound :integer
+#  contact_email    :string
+#
+
 class Job < ApplicationRecord
   validates :title, presence: true
 end
