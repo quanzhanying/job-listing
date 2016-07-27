b3c7ed6e246776465b7c94eaa0c4d824e7db33f2
chpwang <chpengwang@gmail.com>
2016-07-27 13:11:29 +0800

    fix a minor bug - update in admin panel

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -26,6 +26,7 @@ class Admin::JobsController < ApplicationController
 	end
 
 	def update
+		@job = Job.find(params[:id])
 		
 		if @job.update(job_params)
 			redirect_to admin_jobs_path, notice: "Updated"
