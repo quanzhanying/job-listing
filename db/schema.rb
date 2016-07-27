a8a3ab3cfab9e49a551ec1b61ae786dd42e0209e
linzhewei <zp8106@gmail.com>
2016-07-27 12:47:23 +0800

    some change

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726145315) do
+ActiveRecord::Schema.define(version: 20160727020249) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_bound"
     t.         "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
