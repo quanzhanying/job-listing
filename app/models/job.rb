4c8b80590450cfc5d07ef8153b1844c6c25826f8
liyanzeng <yanzengli@gmail.com>
2016-07-27 13:03:21 +0800

    implement step3

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,6 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
 end
