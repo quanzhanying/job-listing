90bddd9fb4511ad7f22db87821394f212048114f
Jone Doe <johndoe@example.com>
2016-07-27 13:14:07 +0800

    implement job CURD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :jobs
   root 'welcome#index'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
