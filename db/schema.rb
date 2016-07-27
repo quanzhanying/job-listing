63f4c385833ac390970f8857df97b81112c82f7f
Rebecca Wang <wangmmqq@gmail.com>
2016-07-27 18:46:57 +0800

    implement part 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727080759) do
+ActiveRecord::Schema.define(version: 20160727092252) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.         "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
