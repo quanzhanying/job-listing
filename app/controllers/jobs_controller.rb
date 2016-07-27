bfc9c9242276ad7464c7dcda5a1ab1e7186e7b4e
nfreeness <nfreeness@gmail.com>
2016-07-27 20:56:01 +0800

    finish step 3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
     before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-    
+
   def show
     @job = Job.find(params[:id])
   end
@@ -46,7 +46,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 end
