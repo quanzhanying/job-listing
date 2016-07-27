d3bc4a5f6fd7ee60bc76191d4984187660d195b2
huanuan <xieping7006@gmail.com>
2016-07-27 19:14:36 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def admin?
+     email == 'xdite@growth.school'
+  end
+  def admin?
+    is_admin
+  end       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :jobs
+
+  namespace :admin do
+    resources :jobs
+  end
+  
   root 'jobs#index'
 end
