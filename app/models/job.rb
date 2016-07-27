a1aa9bbcc67939c215ebdff18184884942fbad27
shaojunda <shaojunda@gmail.com>
2016-07-27 14:19:18 +0800

    change wage_upper_bound's validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -17,9 +17,9 @@
 class Job < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: {greater_than: 0}
+  validates :wage_upper_bound, presence: true, numericality: {greater_than: 0}
+  validates :wage_lower_bound, presence: true, numericality: {greater_than: 0}
+  # validates :wage_lower_bound, numericality: {greater_than: 0}
 
   scope :show_public, -> { where("is_hidden = 'false'") } # :is_hidden => false
   scope :recent, -> { order(created_at: :desc) }
