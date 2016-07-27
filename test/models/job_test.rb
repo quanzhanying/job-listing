afd96c5b43c696ad7782ac2659416d6e8066556b
linzhewei <zp8106@gmail.com>
2016-07-27 17:17:09 +0800

    change

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  description      :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  wage_upper_bound :integer
+#  wage_lower_bound :iteger
+#  contact_email    :string
+#  is_hidden        :boolean          default(TRUE)
+#
+
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
