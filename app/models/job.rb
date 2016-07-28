39f43dc3a065639f243cd7aaa71f8fa422b9f826
chpwang <chpengwang@gmail.com>
2016-07-28 11:02:44 +0800

    wage upper/lower bound is limited to number

--- app/models/job.rb
+++ app/models/job.rb
@@ -15,4 +15,8 @@
 class Job < ApplicationRecord
 	validates :title, presence: true
 	validates :description, presence: true
+
+	validates :wage_lower_bound, numericality: { greater_than: 0}
+	validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound}
+
 end
