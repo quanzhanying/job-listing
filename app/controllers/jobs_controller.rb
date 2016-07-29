3649d1ee61ed2326428bc8cbd1818bd1ca8acf43
xdite <xuite.joke@gmail.com>
2016-07-30 01:38:47 +0800

    refactor with scope

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -13,11 +13,11 @@ class JobsController < ApplicationController
   def index
     @jobs = case params[:order]
             when 'by_lower_bound'
-              Job.where(is_hidden: false).order('wage_lower_bound DESC')
+              Job.published.order('wage_lower_bound DESC')
             when 'by_upper_bound'
-              Job.where(is_hidden: false).order('wage_upper_bound DESC')
+              Job.published.order('wage_upper_bound DESC')
             else
-              Job.where(is_hidden: false).order('created_at DESC')
+              Job.published.order('created_at DESC')
             end
   end
 
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,16 +2,17 @@ class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
+  validates :wage_lower_bound, numericality: { greater_than: 0 }
 
+  scope :published, -> { where(is_hidden: false) }
 
   def publish!
     self.is_hidden = false
-    self.save
+    save
   end
 
   def hide!
     self.is_hidden = true
-    self.save
+    save
   end
 end
