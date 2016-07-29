ce5610efc6b7bb1a9838ba0a8bed0f898b1b2565
xdite <xuite.joke@gmail.com>
2016-07-30 01:36:49 +0800

    use case when to order upper_bound

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,49 +5,56 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
 
     if @job.is_hidden
-      flash[:warning] = "This Job already archieved"
+      flash[:warning] = 'This Job already archieved'
       redirect_to root_path
     end
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.where(is_hidden: false).order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.where(is_hidden: false).order('wage_upper_bound DESC')
+            else
+              Job.where(is_hidden: false).order('created_at DESC')
+            end
   end
 
-#  def new
-#    @job = Job.new
-#  end
+  #  def new
+  #    @job = Job.new
+  #  end
 
-#  def create
-#    @job = Job.new(job_params)
+  #  def create
+  #    @job = Job.new(job_params)
 
-#    if @job.save
-#      redirect_to jobs_path
-#    else
-#      render :new
-#    end
-#  end
+  #    if @job.save
+  #      redirect_to jobs_path
+  #    else
+  #      render :new
+  #    end
+  #  end
 
-#  def edit
-#    @job = Job.find(params[:id])
-#  end
+  #  def edit
+  #    @job = Job.find(params[:id])
+  #  end
 
-#  def update
-#    @job = Job.find(params[:id])
-#    if @job.update(job_params)
-#      redirect_to jobs_path
-#    else
-#      render :edit
-#    end
-#  end
+  #  def update
+  #    @job = Job.find(params[:id])
+  #    if @job.update(job_params)
+  #      redirect_to jobs_path
+  #    else
+  #      render :edit
+  #    end
+  #  end
 
-#  def destroy
-#    @job = Job.find(params[:id])
+  #  def destroy
+  #    @job = Job.find(params[:id])
 
-#    @job.destroy
-#
-#    redirect_to jobs_path
-#  end
+  #    @job.destroy
+  #
+  #    redirect_to jobs_path
+  #  end
 
   private
 
