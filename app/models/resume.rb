0a75e04dda66b7113bbd85e212e5cb0181808def
shaojunda <shaojunda@gmail.com>
2016-07-30 23:31:56 +0800

    fix not identify pdf file.

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -4,7 +4,7 @@ class Resume < ApplicationRecord
 
   mount_uploader :attachment, AttachmentUploader
   validates :attachment, file_size: { less_than_or_equal_to: 2.megabyte },
-                             file_content_type: { allow: ['pdf'] }
+                             file_content_type: { allow: 'application/pdf' }
 
   validates :content, presence: true
 
