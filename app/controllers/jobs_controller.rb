4fada8f214d5474625f5ced138d8ee0f40a06bc9
liusining <siningdream@gmail.com>
2016-07-31 14:27:00 +0800

    implement CRUD under the help of textbook

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,59 @@
+class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :find_job, only: [:edit, :update, :show, :destroy]
+
+  def index
+      @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+
+    end
+
+  end
+
+  def edit
+
+
+  end
+
+  def update
+    if @job.update(job_params)
+        redirect_to jobs_path
+    else
+        render :edit
+    end
+
+  end
+
+  def show
+
+
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path
+
+  end
+
+  private
+
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
+
+    def find_job
+      @job = Job.find(params[:id])
+    end
+end
