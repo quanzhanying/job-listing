2a1e6681847c39177e4874e6ac265333d9e97737
liusining <siningdream@gmail.com>
2016-08-01 22:40:25 +0800

    implement resume's attachment

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,22 @@
+class ResumesController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new
+  end
+
+  def create
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new(resume_params)
+    if @resume.save
+      redirect_to job_path(@job), notice: "The resume #{@resume.name} has been uploaded."
+    else
+      render :new
+    end
+  end
+
+  private
+  def resume_params
+    params.require(:resume).permit(:name, :attachment)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731132245) do
+ActiveRecord::Schema.define(version: 20160801110015) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160731132245) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
