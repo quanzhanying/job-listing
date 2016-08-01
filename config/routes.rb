14789fe79e9f6290afb448f9beef4a495cdc71eb
聂弋程 <nieyicheng@e.local>
2016-08-01 19:33:46 +0800

    hahah

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
       end
     end
   end
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
