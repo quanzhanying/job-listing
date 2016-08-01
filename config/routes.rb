2f32a477bc8068806ca542973ea51a0d17342cc3
Rebecca Wang <wangmmqq@gmail.com>
2016-08-01 20:10:11 +0800

    upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :jobs
    end
+
+   resources :jobs do
+     resources :resumes
+   end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
