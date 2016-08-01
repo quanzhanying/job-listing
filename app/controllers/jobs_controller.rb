f073aab53e154b4a35dcd8e987f1669cd3b0bff6
liber17321 <tianwh3721@gmail.com>
2016-08-01 15:35:35 +0800

    implement sort

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,16 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
+
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+           when 'by_lower_bound'
+             Job.where(is_hidden: false).order('wage_lower_bound DESC')
+           when 'by_upper_bound'
+             Job.where(is_hidden: false).order('wage_upper_bound DESC')
+           else
+             Job.where(is_hidden: false).order('created_at DESC')
+           end
   end
 
   def new
