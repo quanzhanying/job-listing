234d08e6d1f7927cbe9e5d21e390853bbd5206f6
chpwang <chpengwang@gmail.com>
2016-08-01 14:45:28 +0800

    fix bugs about User Dashboard functionality

--- app/controllers/applicant/resumes_controller.rb
+++ app/controllers/applicant/resumes_controller.rb
@@ -1,6 +1,7 @@
 class Applicant::ResumesController < ApplicationController
 
 	before_action :authenticate_user!
+	before_action :get_params, only: [:edit, :update]
 	layout 'applicant'
 
 	def index
@@ -11,4 +12,30 @@ class Applicant::ResumesController < ApplicationController
 		@resume = Resume.find(params[:id])
 		@job = @resume.job
 	end
+
+	def edit
+		@job = @resume.job
+	end
+
+	def update
+		
+		@job = @resume.job
+		if @resume.update(resume_params)
+			redirect_to applicant_resumes_path, notice: "Your Resume has been Updated"
+		else
+			render :edit
+		end
+	end
+
+
+	private
+
+	def get_params
+		@resume = Resume.find(params[:id])
+	end
+
+	def resume_params
+		params.require(:resume).permit(:content, :attachment, :name)
+	end
+
 end
