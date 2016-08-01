d85e1078a319fcbb30e087a8a5286469dc7a2b89
nieyicheng <nieyicheng1990@gmail.com>
2016-08-01 15:22:22 +0800

    step1 2 3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,6 +25,7 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    @resumes = @job.resumes
 
     if @job.is_hidden
       flash[:warning] = "This Job already archieved"
