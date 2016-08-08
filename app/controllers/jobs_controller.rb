14de23f98a0f3a91d3f069fc83d90aa37f7d1cec
翁亚东 <wengyadong@macbook.local>
2016-08-08 19:09:22 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,16 @@
 class JobsController < ApplicationController
  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
-  def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
-  end
+ def index
+   @jobs = case params[:order]
+           when 'by_lower_bound'
+             Job.published.order('wage_lower_bound DESC')
+           when 'by_upper_bound'
+             Job.published.order('wage_upper_bound DESC')
+           else
+             Job.published.recent
+           end
+ end
 
   def show
       @job = Job.find(params[:id])
