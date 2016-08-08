d22f395ca9356c127d9aa82d6d5c5de2ac279495
Lanxinn <nlanxin@icloud.com>
2016-08-08 11:48:01 +0800

    implement step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -46,6 +46,21 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+
+    @job.hide!
+
+    redirect_to :back
+  end
+
   private
 
   def job_params
--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,14 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   validates :title, presence: true
+  class Job < ApplicationRecord
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
+
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
 end
