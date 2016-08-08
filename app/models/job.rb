516778921944ce6b6b4d2e37a3efe51eeb3f8b8c
Lanxinn <nlanxin@icloud.com>
2016-08-08 14:08:29 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,12 @@
 class Job < ApplicationRecord
+	has_many :resumes
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   validates :title, presence: true
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
+
   def publish!
     self.is_hidden = false
     self.save
