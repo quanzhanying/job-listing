eaeace0d846ebff7a27219724b450559f72cc7d5
翁亚东 <wengyadong@macbook.local>
2016-08-08 15:29:25 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -47,6 +47,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :dascription)
+    params.require(:job).permit(:title, :dascription, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
