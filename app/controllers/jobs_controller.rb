6825f7c212325dd362b9b7de49e1cfa08ac565e2
shaheming <hemingsha@gmail.com>
2017-01-03 16:42:28 +0800

    add salary boundary and contact to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -46,7 +46,7 @@ class JobsController < ApplicationController
 private
 
 def job_params
-	params.require(:job).permit(:title,:description)
+	params.require(:job).permit(:title,:description,:wage_upper_bound,:wage_lower_bound,:contact_email)
 end
 
 # def require_is_admin
