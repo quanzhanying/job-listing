ec67fc3b0c683da8bc761c1010704a4a13d4132c
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-03 11:17:46 +0800

    建立controller，所有html页面

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,40 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+  def new
+    @job = Job.new
+  end
+  def show
+    @job = Job.find(params[:id])
+  end
+  def edit
+    @job = Job.find(params[:id])
+  end
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+    redirect_to jobs_path, notice: "Update Success"
+  else
+    render :edit
+  end
+  end
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job deleted"
+  end
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
