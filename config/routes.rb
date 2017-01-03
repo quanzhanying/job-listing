ed5bf58bb450e48f468e595161baa3527719b143
你的名字 <email>
2017-01-03 11:08:29 +0800

    create jobs index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-   root to: "welcome#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :jobs
+  root 'jobs#index'
 end
