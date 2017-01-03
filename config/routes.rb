3142566ea085602e12ea4dbc76330a3d92a1f95c
Yammy <yammyxing@foxmail.com>
2017-01-03 19:39:28 +0800

    add admin/jobs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :jobs
   root 'jobs#index'
+
+  namespace :admin do
+    resources :jobs
+  end
   # root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
