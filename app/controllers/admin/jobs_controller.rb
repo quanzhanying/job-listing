894bf6a0d5cf0dd1be44fa0655af6537977a6173
Yammy <yammyxing@foxmail.com>
2017-01-03 22:12:46 +0800

    add wage_upper_bound and lower_bound and contact email to admin input

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -42,17 +42,17 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
-  def require_is_admin
-    # if current_user.email != 'abc@happy.com'
-    if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
+  # def require_is_admin
+  #   # if current_user.email != 'abc@happy.com'
+  #   if !current_user.admin?
+  #     flash[:alert] = 'You are not admin'
+  #     redirect_to root_path
+  #   end
+  # end
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
