bc842bdc2a063a519da3b15dce67d504c1f39881
milesmao233 <milesmao233@gmail.com>
2017-01-03 21:10:29 +0800

    install simpleform with bootstrap

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,5 +1,5 @@
 module FlashesHelper
-  FLASH_CLASSES = { alert: "danger" notice: "success", warning: "warning"}.freeze
+  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
