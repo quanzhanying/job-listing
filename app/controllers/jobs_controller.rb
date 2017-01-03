9839e1bac4686b488a73099a1cfc04c937a07eb3
shaheming <hemingsha@gmail.com>
2017-01-03 15:53:48 +0800

    implement basic grud as well as authenticate

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,52 @@
 class JobsController < ApplicationController
+	before_filter :authenticate_user!,only: [:new,:create,:destroy,:edit]
 	def index
 		@jobs= Job.all
 	end
+
+	def show
+		@job= Job.find(params[:id])
+	end
+
+	def new
+		@job= Job.new
+	end
+
+	def create
+		@job= Job.new(job_params)
+		if @job.save
+			redirect_to jobs_path, notice:"Create a job successfully!"
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@job= Job.find(params[:id])
+	end
+
+	def update
+		@job= Job.find(params[:id])
+		if @job.update(job_params)
+			redirect_to jobs_path, warning: "Update successfully!"
+		else
+			render :edit
+		end	
+	end
+
+
+	def destroy
+		@job= Job.find(params[:id])
+		if @job.destroy
+			redirect_to jobs_path, alert: "Delete successfully!"
+		end
+	end
+
+private
+
+def job_params
+	params.require(:job).permit(:title,:description)
+	
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-root 'welcome#index'
+root 'jobs#index'
 resources :jobs
 end
