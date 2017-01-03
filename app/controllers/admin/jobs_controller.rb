539fea726faa776035b3fde9ac13d340a3ed1cbd
Yammy <yammyxing@foxmail.com>
2017-01-03 21:38:24 +0800

    use admin and define admin in user.rb

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -43,7 +43,8 @@ class Admin::JobsController < ApplicationController
   end
 
   def require_is_admin
-    if current_user.email != 'abc@happy.com'
+    # if current_user.email != 'abc@happy.com'
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
