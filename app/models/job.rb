0b89f8f92c8e1b0bc5e1d96111c31f98039b0a7e
megy chen <megy.chen203@gmail.com>
2017-01-04 16:22:40 +0800

    add wage and email to job and do some validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
-
-  
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0 }
 end
