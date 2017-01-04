6da3f92920ddbfa09d331ccfb38b8762da19f6fc
lacfo <liyuanlacfo1990@gmail.com>
2017-01-04 16:21:03 +0800

    Validate the wage to be present

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ApplicationRecord
 	validates :title, :description, presence: true
+	validates :title, :wage_lower_bound, presence: true
+	validates :title, :wage_upper_bound, presence: true
 	validates :wage_lower_bound, numericality: { greater_than: 0 }
 end
