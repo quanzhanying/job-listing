3050c6918f2eaca8bee99112257c4fcec84a357f
xieyanlin <xieyanlin820@gmail.com>
2017-01-04 18:31:24 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
 
 def admin?
-  email == "1@qq.com"
+  is_admin
 end
 end
