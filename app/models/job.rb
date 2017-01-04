e4c0410a4f42144c5b55a1ef0844ecf16fedc28b
lacfo <liyuanlacfo1990@gmail.com>
2017-01-04 16:04:02 +0800

    Add wage_lower_bound, wage_upper_bound and contace_email to the job model and validate lower bound greater than 0

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
 	validates :title, :description, presence: true
+	validates :wage_lower_bound, numericality: { greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104072905) do
+ActiveRecord::Schema.define(version: 20170104075031) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.decimal  "wage_upper_bound"
+    t.decimal  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
