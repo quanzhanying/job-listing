0613668974a8894751fb6d97bdf062e7e618433b
lacfo <liyuanlacfo1990@gmail.com>
2017-01-04 12:44:33 +0800

    Add jobs#new and jobs#create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,23 @@ class JobsController < ApplicationController
 	def index
 		@jobs = Job.all
 	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(job_params)
+		if @job.save
+			redirect_to jobs_path, notice: "Create a new job"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def job_params
+		params.require(:job).permit(:title, :description)
+	end	
 end
