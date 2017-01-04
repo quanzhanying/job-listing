7a2e6f319403749f899481ae09f11fc6d54b35a4
eliza chen <elizachen34@gmail.com>
2017-01-04 17:29:00 +0800

    implement admin CRUD by myself, before compare to teacher's answer.

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,53 @@
 class Admin::JobsController < ApplicationController
-  before_action :authenticate_user!
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
+    @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      flash[:notice] = "create job successful"
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find([:id])
+    if @job.update(job_params)
+      flash[:notice] = "update job successful"
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @job = Job.find([:id])
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+
+    flash[:alert] = "delete successful"
+    redirect_to admin_jobs_path
+  end
+
+  private
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
 
 end
