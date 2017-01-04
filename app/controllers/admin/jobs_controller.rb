141081197aacf0c12ba7ec98e37a1eaaed2fe69c
miaoxiaoge <taketimeasafriend@gmail.com>
2017-01-05 03:50:14 +0800

    add admin?

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,8 +1,17 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_is_admin
 
   # 首页
   def index
     @jobs = Job.all
+  end
+
+  # 管理员确认
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "你不是管理员！"
+      redirect_to root_path
+    end
   end
 end
