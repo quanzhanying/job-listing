81d140910be4fff6cc78912201be5ef07f467313
megy chen <megy.chen203@gmail.com>
2017-01-04 15:41:29 +0800

    add current_user to admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-
+  before_filter :require_is_admin
 
   def index
     @jobs = Job.all
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,10 +2,5 @@ class Job < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = "You are not admin"
-      redirect_to root_path
-    end
-  end
+  
 end
