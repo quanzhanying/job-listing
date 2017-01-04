08e912f6f7d94b6fbd660a4a369a8be74753b965
FreshGeek001 <memory2026@126.com>
2017-01-04 16:19:11 +0800

    add admin access limitation

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
 
     def show
       @job = Job.find(params[:id])
@@ -43,6 +44,13 @@ class Admin::JobsController < ApplicationController
 
       redirect_to admin_jobs_path
     end
+
+    def require_is_admin
+    if current_user.email != 'memory2026@126.com'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
 
     private
 
