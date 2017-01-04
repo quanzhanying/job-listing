96a87694d1561a5c94843bffd285f0e6d8483c5c
xieyubai <xieyubai17@gmail.coml>
2017-01-04 22:23:11 +0800

    add show and index 功能和html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   devise_for :users
+
+  resources :jobs
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
