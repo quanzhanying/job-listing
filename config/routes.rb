d567200208c8028ba626eaf1d3dd7e1a1d2a114e
fanliangchun <jeremydo2010@gmail.com>
2017-01-04 20:58:04 +0800

    replace root with jobs#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :jobs
   devise_for :users
-  root 'welcome#index'
+  root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
