02f2981be2f5a259924bc40a0d5fb8cb2bed27f7
FreshGeek001 <memory2026@126.com>
2017-01-04 21:09:09 +0800

    wage can't be blank

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -48,5 +48,6 @@ class Admin::JobsController < ApplicationController
     private
 
     def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)    
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  end  
     end
