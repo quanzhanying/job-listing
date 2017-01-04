5861d702afbf0005d6ea0c9ff199065b0ed6b154
megy chen <megy.chen203@gmail.com>
2017-01-04 13:21:46 +0800

    implement jobs#destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -34,6 +34,12 @@ class JobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job Deleted"
+  end
+
   def job_params
     params.require(:job).permit(:title, :description)
   end
