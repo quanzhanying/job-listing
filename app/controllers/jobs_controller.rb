967e2e6de245b2572e32e67dc482cc94420cf898
apple <apple@appledeMacBook-Pro.local>
2017-01-04 11:38:02 +0800

    add admin/jobs/intex.html

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -41,7 +41,18 @@ class JobsController < ApplicationController
     redirect_to jobs_path
 
   end
+
+  def require_is_admin
+    if  !current_user.admin?
+      flash[:alert] = "You are not admin!"
+      redirect_to root_path
+    end
+  end
+
   private
+
+
+
 
   def job_params
     params.require(:job).permit(:title,:description)
