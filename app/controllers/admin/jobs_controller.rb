aa5049f4cda85980f3f015a13e50f1238b6d21fc
FreshGeek001 <memory2026@126.com>
2017-01-04 16:23:34 +0800

    change admin condition

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -46,7 +46,7 @@ class Admin::JobsController < ApplicationController
     end
 
     def require_is_admin
-    if current_user.email != 'memory2026@126.com'
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    email == 'memory2026@126.com'
+  end
 end
