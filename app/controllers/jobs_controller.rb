c1c6c11ca9816724a2b73418ba9eefcbc70a5e55
zhangyan <tongkusat123@gmail.com>
2017-01-04 23:04:15 +0800

    implement update and destory action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -26,6 +26,29 @@ class JobsController < ApplicationController
 
   end
 
+  def update
+  @job =Job.find(params[:id])
+
+  if @job.update(job_params)
+  redirect_to jobs_path
+  else
+    render :edit
+  end
+
+  end
+
+
+
+
+  def destroy
+  @job = Job.find(params[:id])
+  @job.destroy
+
+  redirect_to jobs_path
+
+  end
+
+
 private
 
 def job_params
