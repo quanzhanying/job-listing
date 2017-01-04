d2d54ebcd9f6a90ae46d91d102a6f3dd5c787e00
huazai <470021389@qq.com>
2017-01-04 16:53:45 +0800

    implement jobs#edit and update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,6 +18,15 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+      redirect_to jobs_path
+  end
 
   private
 
