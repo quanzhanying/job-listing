e9c6879065ec13ed457e12c715ce075ca2f0584f
fanliangchun <jeremydo2010@gmail.com>
2017-01-04 19:52:05 +0800

    install devise

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
+gem 'devise', '~> 4.2'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,9 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+
+  # needed for devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
