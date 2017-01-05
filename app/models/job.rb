15e956ae89cb5a74da7aaec8e66dfcbcc909dfe1
elizachen <elizachen34@gmail.com>
2017-01-05 08:22:01 +0800

    modify the view for the wage & email info.

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,5 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
-  validates :wage_upper_bound, numericality: { greater_than: wage_lower_bound}
+  validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound}
 end
