df6166d9bdcf2035398e69f9bc904bd873dce66d
megy chen <megy.chen203@gmail.com>
2017-01-05 17:06:32 +0800

    add hidden and public for index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @jobs = Job.all.where(:is_hidden => false)
+    @jobs = Job.all.where(:is_hidden => false).recent
   end
   def show
     @job = Job.find(params[:id])
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,6 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0 }
+
+  scope :recent, -> { order("created_at DESC") }
 end
