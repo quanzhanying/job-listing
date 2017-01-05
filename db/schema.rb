a05d0be8f78cbf5bee5b757d6d09015534154d35
ruiqing0706 <2944925501@qq.com>
2017-01-05 19:49:04 +0800

    Add attachment upload column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105111439) do
+ActiveRecord::Schema.define(version: 20170105114038) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170105111439) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
