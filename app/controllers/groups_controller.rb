4f951fcbcb459023f176b8f13f1f933e39ae5c43
maizi <mww0595@gmail.com>
2017-01-05 15:31:16 +0800

    路人没有权限修改删除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_admin! , only: [:new, :create]
+  before_action :authenticate_admin! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -15,6 +15,10 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+
+    if current_admin != @group.admin
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
   def create
@@ -28,23 +32,31 @@ class GroupsController < ApplicationController
   end
 end
 
-  def update
-       @group = Group.find(params[:id])
+def update
+  @group = Group.find(params[:id])
 
-       if @group.update(group_params)
+  if current_admin != @group.admin
+    redirect_to root_path, alert: "You have no permission."
+  end
 
-       redirect_to groups_path, notice: "Update Success"
-     else
-       render :edit
-     end
-   end
+  if @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+  else
+    render :edit
+  end
+end
 
-     def destroy
-     @group = Group.find(params[:id])
-     @group.destroy
-     flash[:alert] = "Group deleted"
-     redirect_to groups_path
-   end
+def destroy
+  @group = Group.find(params[:id])
+
+  if current_admin != @group.admin
+    redirect_to root_path, alert: "You have no permission."
+  end
+
+  @group.destroy
+  redirect_to groups_path, alert: "Group deleted"
+end
+
 
   private
 
