c70fcff00435e77cc84c72e75c50e6a362899abb
你的名字 <ksalsqzhbl@aliyun.com>
2017-01-05 12:56:56 +0800

    implement part 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :jobs
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :jobs
+  end
+  
 end
