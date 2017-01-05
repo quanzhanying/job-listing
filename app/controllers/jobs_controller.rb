5839d2fe941feb4a009622d499562f9a858157f7
megy chen <megy.chen203@gmail.com>
2017-01-05 15:03:36 +0800

    render form with partial

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @jobs = Job.all
@@ -32,6 +33,12 @@ class JobsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job deleted!"
   end
 
   def job_params
