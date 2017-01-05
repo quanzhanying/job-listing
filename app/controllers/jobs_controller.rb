fda8252a9ca2e2e45a77e8ef2475da95c1170ff8
xieyanlin <xieyanlin820@gmail.com>
2017-01-06 00:45:16 +0800

    implement simple form and job CRUD

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,41 @@
+class JobsController < ApplicationController
+  before_filter :authenticate_user! ,only: [:new,:create,:edit,:destroy]
+  def index
+    @jobs = Job.all
+  end
+  def new
+    @job = Job.new
+  end
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+    redirect_to jobs_path
+  else
+    render :new
+  end
+end
+  def update
+    @job = Job.find(params[:id])
+    if @Job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    if @job.destroy
+      redirect_to jobs_path,alert: "Job delete"
+ end
+ end
+
+private
+def job_params
+  params.require(:job).permit(:title,:description)
+end
+end
