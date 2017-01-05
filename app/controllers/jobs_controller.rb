72958a419961ae8340309e4dee127d427d54ef94
Antarts <m.dream@foxmail.com>
2017-01-06 03:16:26 +0800

    add validation to title apply to new/create&edit/update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,14 +17,20 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-    redirect_to jobs_path
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   def update
     @job = Job.find(params[:id])
-    @job.update(job_params)
-    redirect_to jobs_path, nitice: 'Update Success'
+    if @job.update(job_params)
+      redirect_to jobs_path, nitice: 'Update Success'
+    else
+      render :edit
+    end
   end
 
   def destroy
