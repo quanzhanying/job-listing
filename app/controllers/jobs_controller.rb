f8bfc24db0ea1472da44f2761f6e8ac567d864c8
王巍 <wei.wang90@foxmail.com>
2017-01-05 21:29:35 +0800

    implemetion job new/create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,27 @@
 class JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :Destroy]
+
   def index
     @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 end
