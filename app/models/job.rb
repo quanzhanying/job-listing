223d2a88f978cff5ebe1fdc68df05260e00f1103
ruiqing0706 <2944925501@qq.com>
2017-01-05 18:15:58 +0800

    refactor with scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,14 +2,16 @@ class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
+  validates :wage_lower_bound, numericality: { greater_than: 0 }
+
+  scope :published, -> { where(is_hidden: false) }
 
   def hide!
     self.is_hidden = true
-    self.save
+    save
   end
   def publish!
     self.is_hidden = false
-    self.save
+    save
   end
 end
