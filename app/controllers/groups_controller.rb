23aa74bff0092e1c2d5e0446a19e26faed99fcde
maizi <mww0595@gmail.com>
2017-01-05 13:00:39 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,7 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  def new
+    @group = Group.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
    resources :groups
-   root 'welcome#index'
+   root 'groups#index'
 end
