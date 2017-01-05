180fce796d8b93f4d6aa9acdb22250307429299b
shiyongheng <ziyou@shiyonghengdeMacBook-Air.local>
2017-01-05 21:00:26 +0800

    step3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -47,7 +47,8 @@ class JobsController < ApplicationController
 	private 
 
 	def job_params
-		params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+		params.require(:job).permit(:title, :description, 
+			:wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
 	end
 
 
