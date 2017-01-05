186f63466f0c4b5510442fdfcfec6f7c6d3cb141
shiyongheng <ziyou@shiyonghengdeMacBook-Air.local>
2017-01-05 18:42:25 +0800

    不是管理员，进不去后台

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,13 @@
 class Admin::JobsController < ApplicationController
 	before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+	before_filter :require_is_admin
+
+	def require_is_admin
+		if current_user.email !='xdite@growth.school'
+			flash[:alert] = 'You are not admin'
+			redirect_to root_path
+		end
+	end
 
 	def show
 		@job = Job.find(params[:id])
