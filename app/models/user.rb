82748be5bcc9b4fc1f2b8834f52e64ab6df3f6dc
cuishirui <344494024@qq.com>
2017-01-05 11:31:22 +0800

    创建管理者账号

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def admin?
+    email == '18551038995@163.com'
+  end
 end
