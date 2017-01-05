0c874503aa84f96a3a564d0fe327bc118a8dcd46
makun <michaelmakun@aliyun.com>
2017-01-05 21:12:42 +0800

    implement jobs#create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,40 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "update success"
+    else
+      render :edit
+    end
+
+  end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+
+  end
+
 end
