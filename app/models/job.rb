59018e706ad2b885b91079822ea5b1e4c85083dd
eliza chen <elizachen34@gmail.com>
2017-01-05 11:32:13 +0800

    finnally, fix the bug: simple_format cannot be blank, it's controller error, the job_params, must permit wage_upper_bound, wage_lower_bound, and email.

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,5 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
   validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound}
-
   belongs_to :user
 end
