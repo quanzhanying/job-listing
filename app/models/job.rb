a8228a25fa30c1f37ec1d8659712e80d88865e1f
ruiqing0706 <2944925501@qq.com>
2017-01-05 19:08:19 +0800

    refactor with recent scope

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,7 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, numericality: { greater_than: 0 }
 
   scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order("created_at DESC") }
 
   def hide!
     self.is_hidden = true
