4c0810b3dff135d529da1f8044a7966917731bce
tuantuan <lisuji1986@gmail.com>
2017-01-05 23:23:38 +0800

    Job crud and devise system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    flash[:notice] = "ceshi"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'jobs#index'
+  resources :jobs
 end
