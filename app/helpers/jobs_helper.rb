c19c0151ddcd686fc4208a733be35e5319bf41c0
miaoxiaoge <taketimeasafriend@gmail.com>
2017-01-05 23:58:22 +0800

    job_helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,4 +2,12 @@ module JobsHelper
   def render_job_description(job)
     simple_format(job.description)
   end
+
+  def job_status(job)
+    if job.is_hidden
+      "(🕶隐藏)"
+    else
+      "(👓可见)"
+    end
+  end
 end
