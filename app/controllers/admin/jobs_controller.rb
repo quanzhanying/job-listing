dc51456925e4520a9cf7efec1ba545a236b995ec
eliza chen <elizachen34@gmail.com>
2017-01-05 16:46:25 +0800

    add a blank sidebar & admin.html.erb

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,6 +2,8 @@ class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_filter :require_is_admin
 
+  layout "admin"
+
   def index
     #@jobs = Job.all
     @jobs = Job.order("created_at DESC")
