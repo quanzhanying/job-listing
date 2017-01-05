c276745c2d85dc045838233309d24cb5ee2f860d
makun <michaelmakun@aliyun.com>
2017-01-05 19:56:27 +0800

    replace root with jobs#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :jobs
-  root 'welcome#index'
+  root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
