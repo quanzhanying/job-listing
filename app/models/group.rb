1aae8e1a0228f2e0858dd5b900a7e1a8b9bebf88
maizi <mww0595@gmail.com>
2017-01-05 19:42:52 +0800

    薪资栏不能为空，下限不低于0

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ActiveRecord::Base
   belongs_to :admin
  validates :title, presence: true
+ validates :up, presence: true
+ validates :down, presence: true
+ validates :down, numericality: { greater_than: 0}
+ 
 end
