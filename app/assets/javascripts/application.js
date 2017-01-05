f5901c05df9a8066d26c1979c664621dc4426ad5
fanliangchun <jeremydo2010@gmail.com>
2017-01-06 06:09:16 +0800

    add bootstarp flash function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap/alert
 //= require_tree .
