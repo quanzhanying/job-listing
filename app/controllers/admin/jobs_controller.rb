705cc6eb973e674b59ad04dc61b7eb812ab7bd4c
王巍 <wei.wang90@foxmail.com>
2017-01-05 22:37:55 +0800

    implement user method admin?

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -3,7 +3,7 @@ class Admin::JobsController < ApplicationController
   before_filter :require_is_admin
 
   def require_is_admin
-    if current_user.email != 'wei.wang90@foxmail.com'
+    unless current_user.admin?
       redirect_to root_path, alert: "You are not admin"
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-    email = "wefd@dfs"
+    email == "wei.wang90@foxmail.com"
   end
 end
