93eeae997987143332a8e00a92241445c4f09d01
ruiqing0706 <2944925501@qq.com>
2017-01-05 19:43:44 +0800

    add attachment to resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -14,5 +14,7 @@ class Resume < ApplicationRecord
   belongs_to :user
   belongs_to :job
 
+  mount_uploader :attachment, AttachmentUploader
+
   validates :content, presence: true
 end
---
+++ db/migrate/20170105114038_add_attachment_to_resume.rb
@@ -0,0 +1,7 @@
+class AddAttachmentToResume < ActiveRecord::Migration[5.0]
+  def change
+    add_column :resumes, :attachment, :string
+
+    end
+  end
+end
