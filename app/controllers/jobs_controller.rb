4dd204e68cf7cd50e1aca460a110e848d245ae94
你的名字 <ksalsqzhbl@aliyun.com>
2017-01-05 19:28:45 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,7 @@ before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
@@ -45,7 +45,7 @@ before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 end
