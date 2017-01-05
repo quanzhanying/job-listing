59d29bddad907516cfd1e7e4c8b6e6661a1eff0d
lacfo <liyuanlacfo1990@gmail.com>
2017-01-05 08:42:49 +0800

    Order the index page created_at DESC

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,7 +2,7 @@ class Admin::JobsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :require_is_admin
 	def index
-		@jobs = Job.all
+		@jobs = Job.all.order("created_at DESC")
 	end
 
 	def show
