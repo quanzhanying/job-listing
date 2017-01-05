ee4fc689e7f34a701603d4090abc83f6ebb86baf
megy chen <megy.chen203@gmail.com>
2017-01-05 14:42:49 +0800

    implement jobs' new & create action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,22 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
   belongs_to :user, required: false
+  validates :title, presence: true
 end
