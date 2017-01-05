733e1bceedefef6d38badfcb487e77ff582f83c2
hukui <tckuikui1234>
2017-01-06 06:13:39 +0800

    add job'show action and view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,11 @@
 class JobsController < ApplicationController
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
