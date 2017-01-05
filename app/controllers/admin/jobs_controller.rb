a6faf78c66b72b603b7fba8823e872a681d5a844
megy chen <megy.chen203@gmail.com>
2017-01-05 15:42:12 +0800

    implement admin/jobs CRUD

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,47 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @jobs = Job.all
+  end
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path, alert: "Job deleted!"
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
