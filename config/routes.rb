168c28e3827465da6d58bb47e4d3498fcc1bbc59
bruceqiao <bruceq619@gmail.com>
2017-01-05 11:01:22 +0800

    add bootstrap flash function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'welcom#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
