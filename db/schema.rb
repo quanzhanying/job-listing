c9707e34c26061f93ec31f4047f27efee9711e2c
Antarts <m.dream@foxmail.com>
2017-01-06 23:05:08 +0800

    add hiddent to job

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106142539) do
+ActiveRecord::Schema.define(version: 20170106150144) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_bound"
     t.         "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
