2eaea30a1395a604550ce2b909c2de084c6b4da5
Learnor <learnor@gmail.com>
2017-01-06 00:08:10 -0600

    implement part4, add is_hidden to job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -29,7 +29,7 @@ class Admin::JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
-    if @job.update
+    if @job.update(job_params)
       redirect_to admin_jobs_path
     else
       render :edit
@@ -44,10 +44,9 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
-
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,9 @@
 module JobsHelper
+  def render_job_status(job)
+    if job.is_hidden
+      '(Private)'
+    else
+      '(Public)'
+    end
+  end
 end
