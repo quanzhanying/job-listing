f5ab35bbc5e102b078e1ad1bc6426b3903323742
Antarts <m.dream@foxmail.com>
2017-01-06 16:16:27 +0800

    implement admin jobs#new&create

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -4,4 +4,21 @@ class Admin::JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.find(job_params)
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
