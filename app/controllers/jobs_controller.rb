c9041caecb07cc6ed95bf388a027073ac0d40a8f
hukui <tckuikui1234>
2017-01-06 12:49:32 +0800

    implement job destroy action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -33,6 +33,12 @@ class JobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params
