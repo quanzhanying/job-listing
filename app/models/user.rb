fed79c0303747aadf6081c6468b9608780b054b9
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-06 06:56:24 -0500

    redefine the admin's enter limit and put it into model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    email == 'kaihe1989@gmail.com'
+  end
+
+
+
 end
