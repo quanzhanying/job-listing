c103c3415f36158fe4826807f82c491379ed4c23
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-05 21:54:58 -0500

    create jobs#destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -35,6 +35,14 @@ class JobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+
+    redirect_to jobs_path
+  end
+
 
   private
 
