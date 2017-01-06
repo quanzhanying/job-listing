a9b109cb8e6af96bc9e8f0dbac9711e1a5b1040f
xieyanlin <xieyanlin820@gmail.com>
2017-01-06 15:51:07 +0800

    require admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
-  before_filter :authenticate_user!,only: [:new,:create,:edit,:destroy]
+  before_filter :authenticate_user!,only: [:new,:create,:update,:edit,:destroy]
+  before_filter :require_is_admin
   def index
     @jobs = Job.all
   end
@@ -12,7 +13,7 @@ class Admin::JobsController < ApplicationController
   def edit
     @job = Job.find(params[:id])
   end
-  
+
   def create
     @job = Job.new(job_params)
     if @job.save
