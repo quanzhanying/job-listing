c34707656ead4d7580782f9df44e107b8dbedc6a
huazai <470021389@qq.com>
2017-01-06 15:21:46 +0800

    add sidebar to admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
 before_filter :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
 before_filter :require_is_admin
+layout "admin"
 
   def index
     @jobs = Job.all
