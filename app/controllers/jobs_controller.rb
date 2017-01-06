97e9943ab6d3c8064ee03053d76fcf7c4e71fe53
hukui <tckuikui1234>
2017-01-06 08:56:29 +0800

    implement add a new job fuction

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,9 +2,22 @@ class JobsController < ApplicationController
   def index
     @jobs= Job.all
   end
-  
+
   def show
     @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jop_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   private
