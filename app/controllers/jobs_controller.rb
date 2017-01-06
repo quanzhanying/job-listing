e75fa8176e1df974f59f5899c0833f3c5a2d4e7e
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-05 21:30:14 -0500

    create jobs#new and jobs#create“ exit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,9 +3,28 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
-  
+
   def show
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
