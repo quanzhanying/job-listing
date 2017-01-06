51bfb01cb1e59c9da9889a93b1c35cda2a48dd0f
joshuagou <joshuagou@mail.com>
2017-01-06 21:57:28 +0800

    no admin panel

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
 
    def show
      @job = Job.find(params[:id])
@@ -48,5 +49,12 @@ class JobsController < ApplicationController
 
    def job_params
      params.require(:job).permit(:title, :description)
+   end
+
+   def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
    end
 end
