7c78da8e996aa32ab1d9110cff64e14aad3fa78d
ranty <ranty720@gmail.com>
2017-01-06 23:35:11 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  def admin?
+    is_admin
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
