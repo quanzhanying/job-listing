61ff26396dffb7b5820439482d60752caa2f572e
leichaofan <leichaofan2024@gmail.com>
2017-01-06 17:24:26 +0800

    第二部分

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = '你不是管理员，拉出去斩了！'
+      redirect_to root_path
+    end
+  end
 end
