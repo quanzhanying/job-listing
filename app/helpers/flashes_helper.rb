b8d92a4a2a24e8d618bf69b95187df3fcc8f6d0d
yaoyuting <yau0115@gmail.com>
2017-01-06 10:06:00 +0800

    debug

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -2,10 +2,10 @@ module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
   def flash_class(key)
-    FLASH_CLASSES.fecth key.to_sym, key
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
-  def user_facing_falshes
+  def user_facing_flashes
     flash.to_hash.slice "alert", "notice","warning"
   end
 end
