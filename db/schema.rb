2cdafb6075d5bc0c097530565d5973386be672d9
junoliang <junoliang007@gmail.com>
2017-01-07 00:15:22 +0800

    user can login/logout/signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106082001) do
+ActiveRecord::Schema.define(version: 20170106153351) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
