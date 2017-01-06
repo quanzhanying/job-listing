6e48c03e5f0760a81e738bb3a6a7e75870a2b6ac
Antarts <m.dream@foxmail.com>
2017-01-06 18:23:02 +0800

    add admin limit

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -48,11 +48,4 @@ class Admin::JobsController < ApplicationController
   def job_params
     params.require(:job).permit(:title, :description)
   end
-
-  def require_is_admin
-    unless current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-
   def index
     @jobs = Job.all
   end
