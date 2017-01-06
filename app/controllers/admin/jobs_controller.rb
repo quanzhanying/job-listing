1413d53ee844d4adb9a29720a7f3bf9c57be5e0f
hukui <tckuikui1234>
2017-01-06 13:14:01 +0800

    implement jobs_controller all actions and index.html.erb

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,48 @@
+class Admin::JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path, alert: "Job deleted!"
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+end
