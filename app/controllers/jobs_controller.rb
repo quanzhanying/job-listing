b0031f60b478b5661d7d78601638945b88bc2452
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-06 11:38:51 -0500

    add is_hidden to job_params

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -50,6 +50,6 @@ before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
