78542a939fca6c3b1efd1b66534de85f4763cb78
shuhuanlin <dodoku@gmail.com>
2017-01-06 10:40:53 +0800

    implement jobs#create&sho&destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,45 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path, notice: "資料已建立！"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job =Job.find(params[:id])
+
+    if @job.updatge(job_params)
+      redirect_to jobs_path, notice: "資料已更新！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "資料已刪除"
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+  
 end
