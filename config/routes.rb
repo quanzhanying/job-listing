60f813822f4836f67855f6e6387d94ebf14ef16f
sean lin <seanlin0918@gmail.com>
2017-01-07 09:49:05 +1000

    implement part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :jobs
+  end
+
   resources :jobs
   root 'jobs#index'
 end
