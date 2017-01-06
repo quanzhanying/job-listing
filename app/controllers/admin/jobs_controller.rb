04711e75ef3e9335c5bb10816b39a876aa361ac8
xieyubai <xieyubai17@gmail.coml>
2017-01-06 15:02:34 +0800

    controller的job_params加入:is_hidden

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -48,6 +48,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description,:wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description,:wage_upper_bound,
+    :wage_lower_bound, :contact_email，:is_hidden)
   end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -44,6 +44,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title,:description,:wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title,:description,:wage_upper_bound,
+     :wage_lower_bound, :contact_email，:is_hidden)
   end
 end
