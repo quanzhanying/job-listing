c07a9977c092382b8b357a221033fa5c3b53846c
shijunfeng <johnshiluck@gmail.comgit>
2017-01-06 22:25:00 +0800

    implement step 5

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise"
+gem "font-awesome-rails"
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
     resources :jobs
-  namespace :admin do
-    resources :jobs
-  end
+    namespace :admin do
+      resources :jobs do
+        member do
+          post :publish
+          post :hide
+        end
+      end
+    end
   root 'jobs#index'
 end
