06f1be7e6b4117826b411cc003f42254f83d453e
Antarts <m.dream@foxmail.com>
2017-01-06 16:50:28 +0800

    implement admin jobs#edit&update

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -5,6 +5,14 @@ class Admin::JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def new
     @job = Job.new
   end
@@ -18,11 +26,18 @@ class Admin::JobsController < ApplicationController
     end
   end
 
-  def job_params
-    params.require(:job).permit(:title, :description)
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, nitice: 'Update Success'
+    else
+      render :edit
+    end
   end
 
-  def show
-    @job = Job.find(params[:id])
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 end
