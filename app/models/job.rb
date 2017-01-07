07ba507022155005c5820c1fe1bea564c0ffe378
Antarts <m.dream@foxmail.com>
2017-01-07 19:47:37 +0800

    add validates to contact email

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,5 @@ class Job < ActiveRecord::Base
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
+  validates :contact_email, presence: true
 end
