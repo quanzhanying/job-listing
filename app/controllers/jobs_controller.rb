0faca5f5dc59be048a0ef4aac7bc7f85a9eaa2b0
huojun <huojun_hr@sohu.com>
2017-01-08 00:51:34 +0800

    完成jobs的edit和update功能

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,4 +27,17 @@ class JobsController < ApplicationController
     def job_params
       params.require(:job).permit(:title, :description)
     end
+
+    def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
 end
