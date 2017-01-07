a3eaf41d625eb05e42869e6eb81e5ff18ccc4422
lingyou <fl.socialzone@gmail.com>
2017-01-07 14:45:04 +0800

    implement job#new/create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,17 @@ class JobsController < ApplicationController
     def index
       @jobs = Job.all
     end
+    def new
+      @job = Job.new
+    end
+    def create
+      @job = Job.new(job_params)
+      @job.save
+      redirect_to jobs_path
+    end
+
+    private
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
 end
