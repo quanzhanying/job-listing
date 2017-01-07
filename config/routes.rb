50f552a7a33aef49e94dbdf5cd185efbc4cc3c30
sean lin <seanlin0918@gmail.com>
2017-01-07 20:01:18 +1000

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
 
   resources :jobs
