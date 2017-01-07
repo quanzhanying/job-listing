54b5e899e4a2dd211e38b4aee4991b463de180ef
Cheng Ge <chengge@MBP.local>
2017-01-07 15:51:28 -0500

    added a dectection to check if user is admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
     before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_filter :require_is_admin
 
     def index
         @jobs = Job.all
@@ -45,5 +46,12 @@ class Admin::JobsController < ApplicationController
 
     def job_params
         params.require(:job).permit(:title, :description)
+    end
+
+    def require_is_admin
+        if !current_user.admin?
+            flash[:alert] = "You are not admin"
+            redirect_to root_path
+        end
     end
 end
