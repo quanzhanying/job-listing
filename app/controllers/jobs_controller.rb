24acfac803719fbdef615078d1a69efb805163f9
Wei Yang <richard.weiyang@gmail.com>
2017-01-07 15:34:59 +0800

    Create a job from Web

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,4 +11,16 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def create
+    @job = Job.new(job_params)
+    @job.save
+
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
