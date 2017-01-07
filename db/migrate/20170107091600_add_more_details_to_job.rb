c73ad3b072b66eff7c2f3c35898cf6d6baf66cf2
An Li <anli@Ans-Mac-mini.local>
2017-01-07 17:20:53 +0800

    implement part 3

---
+++ db/migrate/20170107091600_add_more_details_to_job.rb
@@ -0,0 +1,7 @@
+class AddMoreDetailsToJob < ActiveRecord::Migration[5.0]
+  def change
+    add_column :jobs, :wage_upper_bound, :integer
+    add_column :jobs, :wage_lower_bound, :integer
+    add_column :jobs, :contact_email, :string
+  end
+end
