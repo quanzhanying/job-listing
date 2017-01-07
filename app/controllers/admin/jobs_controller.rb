9034ea3a2824d5cb38bc0f4aa26865d4f8e045c6
leichaofan <leichaofan2024@gmail.com>
2017-01-08 00:22:51 +0800

    第五部分

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :destroy]
   before_filter :require_is_admin
+  layout "admin"
 
   def show
     @job = Job.find(params[:id])
@@ -17,7 +18,7 @@ class Admin::JobsController < ApplicationController
     end
   end
   def edit
-    @job = Job,find(params[:id])
+    @job = Job.find(params[:id])
   end
   def update
     @job = Job.find(params[:id])
@@ -32,6 +33,20 @@ class Admin::JobsController < ApplicationController
     @job.destroy
     redirect_to admin_jobs_path
   end
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+
+    @job.hide!
+
+    redirect_to :back
+  end
+
   private
   def job_params
      params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
