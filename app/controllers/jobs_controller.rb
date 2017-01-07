86a1e26b399beb5d1b40d9c174300cb747dd73b5
junoliang <junoliang007@gmail.com>
2017-01-07 18:47:26 +0800

    add edit &update action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,11 +21,21 @@ class JobsController < ApplicationController
       render :new
     end
 
-end
+    def edit
+      @job = Job.find(params[:id])
+    end
 
+    def update
+      @job = Job.find(params[:id])
+      if @job.update(job_params)
+        redirect_to jobs_path
+      else
+        render :edit
+      end
+end
     private
 
     def job_params
       params.require(:job).permit(:title, :description)
     end
-  end
+end
