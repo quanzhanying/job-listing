a6f23511e0d96a5cf80537f467169d35f92eff0d
klpk011 <sophieQ011@gmail.com>
2017-01-07 11:23:03 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,5 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  before_filter :require_is_admin
 
     def show
       @job = Job.find(params[:id])
@@ -45,13 +44,8 @@ class Admin::JobsController < ApplicationController
       redirect_to admin_jobs_path
     end
 
-    def require_is_admin
-      if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
 
+    
 
     private
 
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,13 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
+
+  def require_is_admin
+    if !current_user.admin?
+    flash[:alert] = 'You are not admin'
+    redirect_to root_path
+    end
+  end
 
   def index
       @jobs = Job.all
