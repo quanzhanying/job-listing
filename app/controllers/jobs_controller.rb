3afc5c56980080ea233b72397391361d29cfdc13
zengxiangrong <zengxiangrong5261435@gmail.com>
2017-01-07 11:19:22 +0800

    add admin path

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
@@ -25,10 +25,17 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
     @job = Job.find(params[:id])
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+      redirect_to jobs_path
+
+  end
+
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 end
---
+++ db/migrate/20170106073906_add_other_columns__to_job.rb
@@ -0,0 +1,8 @@
+class AddOtherColumnsToJob < ActiveRecord::Migration[5.0]
+  def change
+    add_column :jobs, :wage_upper_bound, :integer
+    add_column :jobs, :wage_lower_bound, :integer
+    add_column :jobs, :contact_email, :string
+    add_column :jobs, :is_hidden, :boolean, default: true
+  end
+end
