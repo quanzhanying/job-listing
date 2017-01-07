546ecd7df8f344c126ba911c32745802d4ff9f32
zengxiangrong <zengxiangrong5261435@gmail.com>
2017-01-07 17:24:30 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,45 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:index]
+  before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
   def index
     @jobs = Job.all
     flash[:notice] = "早安！你好！"
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job deleted"
+  	redirect_to jobs_path
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 end
