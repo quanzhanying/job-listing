f09b31a47a11a4d0a3e100271cffc1567ba9abe1
sfxg27@gmail.com <feixiguang@feixiguangdeMacBook-Pro.local>
2017-01-07 21:05:30 +0800

    implement part4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def require_is_admin
-    if current_user.admin?
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
