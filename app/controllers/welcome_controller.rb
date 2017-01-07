a40c8d0c4e185f48ef33836ed7bc7539d3346750
Wei Yang <richard.weiyang@gmail.com>
2017-01-07 10:27:49 +0800

    install devise gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     flash[:notice] = "Good morning!"
     flash[:alert] = "Good morning!"
