8dd50589c0e1e51672c716875d13f365c4937dc6
baohua <113983633@qq.com>
2017-01-07 19:49:55 +0800

    implement part 3

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -37,7 +37,7 @@ end
 private
 
 def job_params
-  params.require(:job).permit(:title, :description)
-end
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  end
 
 end
