31fb53f5516cce412a76d8ac8b8ebd1102be420d
你的名字 <ksalsqzhbl@aliyun.com>
2017-01-07 23:00:43 +0800

    error

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,9 +1,14 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_filter :require_is_admin
+  layout "admin"
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
 
   def index
@@ -44,6 +49,18 @@ class Admin::JobsController < ApplicationController
 
     redirect_to admin_jobs_path
   end
+
+    def publish
+      @job = Job.find(params[:id])
+      @job.publish!
+      redirect_to :back
+    end
+
+    def hide
+      @job = Job.find(params[:id])
+      @job.hide!
+      redirect_to :back
+    end
 
   private
 
