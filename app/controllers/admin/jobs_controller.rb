3fe5d25b5ba8a5a1b57dca09c616d09d5925786b
lingyou <fl.socialzone@gmail.com>
2017-01-07 17:38:06 +0800

    admin/job#edit/update/destroy

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,17 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
-    @jobs = current_user.applied_jobs
+    @jobs = current_user.released_jobs
   end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path
+  end
+
 end
