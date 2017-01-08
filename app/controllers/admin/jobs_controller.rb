0a8144d4dce9dabc4a1ab2164237121186fbcd1b
曾 祥荣 <zd52614352008@126.com>
2017-01-08 13:42:38 +0800

    Step 3 - 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -6,12 +6,49 @@ class Admin::JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path,notice: "Create Success"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path,notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job deleted"
+    redirect_to admin_jobs_path
+  end
+
   private
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = "You are not admin"
       redirect_to root_path
     end
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,6 @@ class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
   def index
     @jobs = Job.all
-    flash[:notice] = "早安！你好！"
   end
 
   def new
