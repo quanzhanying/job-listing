84c1ecd0a37d6548d0d0a342ee02976f9dc15433
zhanyouxin <zhanyouxin@gmail.com>
2017-01-08 13:08:24 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,42 +1,41 @@
 class JobsController < ApplicationController
-before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  def show
+    @job = Job.find(params[:id])
+  end
 
   def index
     @jobs = Job.all
   end
 
-  def show
-    @job = Job.find(params[:id])
-  end
-
   def new
-    @job = Job.new
+  @job = Job.new
+end
+
+def create
+  @job = Job.new(job_params)
+
+  if @job.save
+    redirect_to jobs_path
+  else
+    render :new
   end
+end
 
-  def create
-    @job = Job.new(job_params)
+def edit
+  @job = Job.find(params[:id])
+end
 
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
+def update
+  @job = Job.find(params[:id])
+  if @job.update(job_params)
+    redirect_to jobs_path
+  else
+    render :edit
   end
+end
 
-    def edit
-    @job = Job.find(params[:id])
-    end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
+def destroy
     @job = Job.find(params[:id])
 
     @job.destroy
@@ -45,9 +44,10 @@ before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   end
 
 
-  private
 
-  def job_params
-    params.require(:job).permit(:title, :description)
-  end
+private
+
+def job_params
+  params.require(:job).permit(:title, :description)
+end
 end
