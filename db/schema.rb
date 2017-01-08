6185956b026ef7837858ef4b51c28e1fb5cb22e3
tuantuan <lisuji1986@gmail.com>
2017-01-09 06:01:56 +0800

    deplement devise:views

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20170107020600) do
     t.boolean  "is_hidden",        default: true
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
