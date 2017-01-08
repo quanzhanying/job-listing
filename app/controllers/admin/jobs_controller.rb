823ecb06f106b56554a3d99fe31dbc078f6148d9
曾 祥荣 <zd52614352008@126.com>
2017-01-08 23:26:44 +0800

    Step 3 - 4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,5 @@
 class Admin::JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy, :show]
   before_action :require_is_admin
 
   def index
