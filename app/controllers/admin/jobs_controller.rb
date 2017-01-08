3518e7d301157cf85f8aa522a8744e62071b86f4
huojun <huojun_hr@sohu.com>
2017-01-08 15:10:35 +0800

    Step 9: 把 require_is_admin 移到 application_controller

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -51,10 +51,4 @@ class Admin::JobsController < ApplicationController
     params.require(:job).permit(:title, :description)
   end
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
 end
