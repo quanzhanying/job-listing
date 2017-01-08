e25c9ecc3f438edd48f310c288c92d4d84fbb9ba
Yammy <yammyxing@foxmail.com>
2017-01-08 20:52:56 +0800

    add wage_upper_bound and wage_lower_bound and contact_email to add_new_job page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108122216) do
+ActiveRecord::Schema.define(version: 20170108123947) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
