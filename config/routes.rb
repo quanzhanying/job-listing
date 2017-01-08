dfc2e7dc012c6c74d5d93658b1b7bf35de65abf2
shuhuanlin <dodoku@gmail.com>
2017-01-08 12:55:48 +0800

    admin/job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :jobs
+  end
   resources :jobs
-  root 'welcome#index'
+  root 'jobs#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
