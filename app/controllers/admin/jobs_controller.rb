3cf0fe57415bda56b14b7399fb8b82ad752dacc2
你的名字 <email>
2017-01-08 16:36:11 +0800

    part6-3 down

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -41,14 +41,14 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
-  
+
 
 
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 
