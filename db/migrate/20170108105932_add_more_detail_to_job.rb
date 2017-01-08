0a6260f123436b79556620f1b9287963080f87ad
junoliang <junoliang007@gmail.com>
2017-01-08 19:14:08 +0800

    add wage & contact email

---
+++ db/migrate/20170108105932_add_more_detail_to_job.rb
@@ -0,0 +1,8 @@
+class AddMoreDetailToJob < ActiveRecord::Migration[5.0]
+  def change
+    add_column :jobs, :wage_upper_bound, :integer
+    add_column :jobs, :wage_lower_bound, :integer
+    add_column :jobs, :contact_email, :string
+  
+  end
+end
