cffc63b258c56a29c8e5dec5e56674e0c22b9ade
yuansq998 <yuansq998@163.com>
2017-01-08 21:34:52 +0800

    implement part 3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -35,6 +35,6 @@ def destroy
 end
 private
 def job_params
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
 end
 end
