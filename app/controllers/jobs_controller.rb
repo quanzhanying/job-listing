f3af98350bd9ee85d6cad3f3f484c293af8b04e1
Yammy <yammyxing@foxmail.com>
2017-01-08 10:02:44 +0800

    add destroy to controller and limit guests to new/create/show/edit/update/destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
+  
   def index
     @jobs = Job.all
   end
@@ -31,6 +33,12 @@ class JobsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
   end
 
   private
