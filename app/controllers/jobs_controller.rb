a8468890bf03a4111045d9555efc34fa61c74f36
“将叫我将军” <d2194499406@gmail.com>
2017-01-08 17:12:34 +0800

    implement jobs#edit update and destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,6 +11,10 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def create
     @job = Job.new(job_params)
     if @job.save
@@ -18,6 +22,21 @@ class JobsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job Deleted"
   end
 
   private
