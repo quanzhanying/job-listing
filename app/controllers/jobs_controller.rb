df7a3a0e67a2904ba5835217eaff5cdc24d46e38
bigfang <bigfang2016@gmail.com>
2017-01-08 12:05:45 +0800

    implement jobs#edit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,6 +11,10 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def create
     @job = Job.new(job_params)
     @job.save
