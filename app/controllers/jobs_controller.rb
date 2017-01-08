9f307fae86c5fb7d64875888ecc0fcea25f44e91
FreshGeek001 <memory2026@126.com>
2017-01-08 10:56:39 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
   def show
      @job = Job.find(params[:id])
   end
@@ -32,6 +34,13 @@ class JobsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+    redirect_to jobs_path
   end
 
   private
