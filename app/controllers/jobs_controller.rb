80b084150bb4f9dfb126817ae649537f0e9e75a6
yuansq998 <yuansq998@163.com>
2017-01-08 22:30:49 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,7 @@ def show
   @job=Job.find(params[:id])
 end
 def index
-  @jobs=Job.all
+  @jobs=Job.where(:is_hidden => false).order("created_at DESC")
 end
 def edit
   @job=Job.find(params[:id])
@@ -35,6 +35,6 @@ def destroy
 end
 private
 def job_params
-  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
 end
 end
