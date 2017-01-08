75ea120a69d733e79ff03698d90fe84a44cbc29a
lingyou <fl.socialzone@gmail.com>
2017-01-08 11:08:04 +0800

    设置is_hidden的默认值为false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107134852) do
+ActiveRecord::Schema.define(version: 20170108023539) do
 
   create_table "job_relationships", force: :cascade do |t|
     t.integer  "job_id"
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(version: 20170107134852) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "user_id"
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
-    t.boolean  "hidden"
+    t.boolean  "hidden",           default: false
   end
 
   create_table "users", force: :cascade do |t|
