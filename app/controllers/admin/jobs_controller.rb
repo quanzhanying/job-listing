07d8f8fa2d5d948ed8ce9c4a5dd4fa83f7b25f3c
Yammy <yammyxing@foxmail.com>
2017-01-08 20:33:53 +0800

    add require_is_admin and change it to judged by migration

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :require_is_admin
 
   def index
     @jobs = Job.all
@@ -40,6 +41,14 @@ class Admin::JobsController < ApplicationController
     @job.destroy
     redirect_to jobs_path
   end
+
+  # def require_is_admin
+  #   # if current_user.email != 'abc@happy.com'
+  #   if current_user.admin?
+  #     flash[:alert] = "Yoou are not admin"
+  #     redirect_to root_path
+  #   end
+  # end
 
   private
 
