b3380abf649df60221e8ed67028e8d6a7caa1340
charvine <crossovermeister@gmail.com>
2017-01-08 14:01:06 +0800

    first homework

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -45,12 +45,7 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
-  def require_is_admin
-  if !current_user.admin?
-    flash[:alert] = 'You are not admin'
-    redirect_to root_path
-  end
-end
+
 
   private
 
