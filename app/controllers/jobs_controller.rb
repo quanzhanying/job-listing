2e63f317b9995ef05cabb8053172eec7e0e3308c
Yammy <yammyxing@foxmail.com>
2017-01-08 09:55:51 +0800

    add CRUD for admin

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,41 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
