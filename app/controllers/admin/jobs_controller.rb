233deccaec2c5ec1e5ccf1382d9803223421493b
huojun <huojun_hr@sohu.com>
2017-01-08 14:48:48 +0800

    Step 5: 重构 admin 条件，放到 model 里面

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -52,7 +52,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def require_is_admin
-    if current_user.email != 'xdite@growth.school'
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
