61eca0147b6a6f2be6c2b90529c90dbd548c4878
huojun <huojun_hr@sohu.com>
2017-01-08 14:30:41 +0800

    限定只有admin才可以进后台

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
 
   def show
     @job = Job.find(params[:id])
@@ -48,5 +49,12 @@ class Admin::JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title, :description)
+  end
+
+  def require_is_admin
+    if current_user.email != 'xdite@growth.school'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
   end
 end
