6859aaee4b686dbf166932ccb2508e1b7cc9b5e7
陶梦迪 <taomengdi99@163.com>
2017-01-09 13:07:08 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def show
     @job = Job.find(params[:id])
   end
