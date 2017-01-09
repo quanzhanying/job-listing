69999957cf72d254f9af39c5980a49010dad55f8
youying0356 <youying0356@qq.com>
2017-01-10 07:48:32 +0800

    implement job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs
-  root 'welcome#index'
+  root 'jobs#index'
 end
