44f90a9e56e9708f0707aa0e6d9233ca4722563d
huojun <huojun_hr@sohu.com>
2017-01-09 10:56:07 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -50,11 +50,4 @@ class Admin::JobsController < ApplicationController
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
-
 end
