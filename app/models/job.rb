71dc64e750c18fde4294ac021cee507734c4ce4d
chris1203 <liuhuiling1203@gmail.com>
2017-01-09 17:38:01 +0800

    implement v2step5

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,13 @@ class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true, numericality: { greater_than: 0}
   validates :wage_lower_bound, presence: true
+  def publish!
+    self.is_hidden = false
+    self.save
+  end
 
+  def hide!
+    self.is_hidden = true
+    self.save
+  end
 end
