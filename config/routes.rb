86446fa3bcc218fcd396c09d100d7d690dcbe0da
sunxiaomi <sunhemin@outlook.com>
2017-01-09 11:19:40 +0800

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :jobs
+  root 'jobs#index'
+
+
+  namespace :admin do
+    resources :jobs
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
