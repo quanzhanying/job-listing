df6ff81451800cc8eff4331699264c6283cf046d
shijunfeng <johnshiluck@gmail.comgit>
2017-01-09 23:12:24 +0800

    implement part3

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ApplicationRecord
-  belongs_to :user
+
   validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, mumericality: {greater_than: 0}
 end
