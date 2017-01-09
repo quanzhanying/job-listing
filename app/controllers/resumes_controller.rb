461ccd444b321fffbb82827788ce6b35d21ae51c
tuantuan <lisuji1986@gmail.com>
2017-01-09 23:17:05 +0800

    upload resume address

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,21 @@
 class ResumesController < ApplicationController
+
+  def new
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new
+  end
+
+  def created
+    @job = Job.find(parms[:job_id])
+    @resume = Resume.new(resume_params)
+    @resume.job = @job
+    @resume.user = current_user
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:job_id, :user_id)
+  end
+  
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,6 @@
 class Resume < ApplicationRecord
   belongs_to :jobs
   belongs_to :user
+
+
 end
