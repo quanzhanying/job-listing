3d716641da34631f3d45653396b8280a63768c74
sunxiaomi <sunhemin@outlook.com>
2017-01-09 13:27:05 +0800

    admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  
+  require_is_admin
 
   def show
     @job = Job.find(params[:id])
