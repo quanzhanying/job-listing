e9703098952ec35554c2777c0cf6fcc57cd7bc75
leichaofan <leichaofan2024@gmail.com>
2017-01-09 23:14:54 +0800

    第五部分后的装修专案

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,9 +1,9 @@
 module JobsHelper
   def render_job_status(job)
     if job.is_hidden
-      content_tag(:span, "", :class => "fa fa-lock")
+      content_tag(:span, "", :class => "fa fa-lock fa-2x")
     else
-      content_tag(:span, "", :class => "fa fa-globe")
+      content_tag(:span, "", :class => "fa fa-globe fa-2x")
     end
   end
 end
