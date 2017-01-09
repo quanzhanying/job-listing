a2f6d8d1daaf2507330acd9ee2663cbd342b3498
brooks <13764390725@139.com>
2017-01-09 08:35:22 +0800

    finished step3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -45,6 +45,6 @@ before_filter :authenticate_user!, only:[:new, :create, :update, :edit, :destroy
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
