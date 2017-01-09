d59f59812141362782e491f019e1a4dbcc9da32e
sunxiaomi <sunhemin@outlook.com>
2017-01-09 16:54:43 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,7 +2,7 @@ class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   require_is_admin
   def require_is_admin
-    if current_user.email != 'sunhemin@outlook.com'
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
