a8729ffd87ec92f0cf8ef11b7ad79c2d1b09fbae
huojun <huojun_hr@sohu.com>
2017-01-09 13:12:46 +0800

    Step 4: 限制已经“被隐藏的工作”，不可以被看到。

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,7 +10,12 @@ class JobsController < ApplicationController
   end
 
   def show
-      @job = Job.find(params[:id])
+    @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
 
   def edit
