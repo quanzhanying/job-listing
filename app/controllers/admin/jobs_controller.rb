4b150bdff581d1560ce7911fc63dea6d1bb3467f
zxb <himddzxb123@gmail.com>
2017-01-09 12:38:39 +0800

    first week

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -50,16 +50,10 @@ class Admin::JobsController < ApplicationController
 
     redirect_to admin_jobs_path
   end
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
-  end
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
-  end
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
-  end
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+
 end
