72354aea5da0ee33de3dab94b8bd47cbac060e47
shijunfeng <johnshiluck@gmail.comgit>
2017-01-09 08:01:27 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,12 +1,14 @@
-class JobsController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+class Admin::JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
 
-  def index
-    @jobs = Job.all
-  end
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  def index
+    @jobs = Job.all
   end
 
   def new
@@ -17,7 +19,7 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
@@ -30,7 +32,7 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :edit
     end
@@ -38,17 +40,14 @@ class JobsController < ApplicationController
 
   def destroy
     @job = Job.find(params[:id])
-
     @job.destroy
-
-    redirect_to jobs_path
-
+    redirect_to admin_jobs_path
   end
 
- private
+  private
 
- def job_params
-   params.require(:job).permit(:title, :description)
- end
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :jobs
+
+  namespace :admin do
+    resources :jobs
+  end
   root "jobs#index"
 end
