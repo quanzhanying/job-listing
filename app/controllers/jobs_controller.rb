d1fc7f6a206fff3ae65953a2dad17bc02ecb87c5
jay.g <jkland927@hotmail.com>
2017-01-10 19:38:50 +0800

    6-1 step9

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,47 @@
+class JobsController < ApplicationController
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(jobs_path)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: 'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Job deleted."
+  end
+end
+
+private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
