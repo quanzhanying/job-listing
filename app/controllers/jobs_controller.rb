16d1392cfaa33d423551afd3f8ad81c195fa40d9
mike562661672 <mike562661672@gmail.com>
2017-01-10 22:20:37 +0800

    step3over

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,7 +15,7 @@ class JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new（job_params）
+    @job = Job.new(job_params)
     if @job.save
       redirect_to jobs_path
     else
@@ -47,6 +47,6 @@ class JobsController < ApplicationController
   private
 
    def job_params
-     params.require(:job).permit(:title, :description)
+     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
    end
 end
