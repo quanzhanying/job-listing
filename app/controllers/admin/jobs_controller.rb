5bfa8eee9d2e3f0dde4d3f9a0bbf2ba3eacf97d1
zengxiangrong <zengxiangrong5261435@gmail.com>
2017-01-10 15:31:54 +0800

    is_admin 改动

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -46,12 +46,6 @@ class Admin::JobsController < ApplicationController
   end
 
   private
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = "You are not admin"
-      redirect_to root_path
-    end
-  end
 
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
          def admin?
-           email == "zeng@gmail.com"
+           is_admin
          end
 end
