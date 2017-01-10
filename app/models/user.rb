aad2d3f3992aa5df48ebe2628e45cd94223f78d8
jay.g <jkland927@hotmail.com>
2017-01-10 21:05:57 +0800

    create an admin system step1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def admin?
+    email == 'jkland927@hotmail.com'
+  end
 end
