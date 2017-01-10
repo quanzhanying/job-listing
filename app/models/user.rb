56dad5a4d7b3db99b0d37f89246eb4891a6c7190
jay.g <jkland927@hotmail.com>
2017-01-10 21:17:09 +0800

    隐藏对游客的下拉菜单中的管理员

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def admin?
-    email == 'jkland927@hotmail.com'
+    is_admin
   end
 end
