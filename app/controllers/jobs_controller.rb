3949a2c5302fb3ffb18a7e2203878e21f3bf23a5
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-11 11:16:49 +0800

    step1

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,40 @@
+class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  def index
+    @jobs = Job.all
+  end
+  def new
+    @job = Job.new
+  end
+  def show
+    @job = Job.find(params[:id])
+  end
+  def edit
+    @job = Job.find(params[:id])
+  end
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to root_path
+  end
+  private
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
