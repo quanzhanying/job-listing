87c3d0d0ecf0162a02147cc1c0f58d34165f6427
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-11 14:21:58 +0800

    crud & title can't be blank

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,48 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path, notice: "cteate success"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "deleted success!"
+  end
+
+
+private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+end
---
+++ app/models/job.rb
@@ -0,0 +1,5 @@
+class Job < ActiveRecord::Base
+
+  validates :title, presence: true
+
+end
