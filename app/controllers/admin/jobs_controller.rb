ded221323be5979197e2b8f8a8782a21a8f488f3
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-11 21:39:50 +0800

    require_is_admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,21 +1,26 @@
-class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
+
+
+
   def index
     @jobs = Job.all
-  end
-
-  def new
-    @job = Job.new
   end
 
   def show
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+  end
+
   def create
     @job = Job.new(job_params)
+
     if @job.save
-      redirect_to jobs_path, notice: "cteate success"
+      redirect_to admin_jobs_path
     else
       render :new
     end
@@ -27,9 +32,9 @@ class JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
 
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
     else
       render :edit
     end
@@ -38,14 +43,14 @@ class JobsController < ApplicationController
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
-    redirect_to jobs_path, alert: "deleted success!"
+    redirect_to admin_jobs_path
   end
 
-
-private
+  private
 
   def job_params
     params.require(:job).permit(:title, :description)
   end
+
 
 end
