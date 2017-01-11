e81a36ecdc9269dfb119e8132699c896ef6f5d6c
mike562661672 <mike562661672@gmail.com>
2017-01-11 22:35:57 +0800

    6-4Step 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,9 +2,7 @@ class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
 
-  
-
-    def show
+      def show
       @job = Job.find(params[:id])
     end
 
