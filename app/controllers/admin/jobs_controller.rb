c66aa42fb9863cd606f01e2c43cf1e3ec8ac32fd
runpee <992142460@qq.com>
2017-01-11 21:53:34 +0800

    first week

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,6 +2,7 @@ class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   before_filter :require_is_admin
+  layout "admin"
 
   def show
     @job = Job.find(params[:id])
