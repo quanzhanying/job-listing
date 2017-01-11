50a2c4fb75782a192f2a32c65fe42cdc09e9ea43
milesmao233 <milesmao233@gmail.com>
2017-01-11 11:52:10 +0800

    implement step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -42,6 +42,20 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path, alert: "job delete"
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+
+      redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+
+     redirect_to :back
+  end
+
   private
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
