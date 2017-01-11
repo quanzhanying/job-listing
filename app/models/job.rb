8c723bb301b4ba6fb2dba7b9fc4ada3abbba4ea1
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-11 15:02:00 +0800

    step3

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,8 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :contact_email, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
+  validates :wage_upper_bound, numericality: { greater_than: 0}
 end
