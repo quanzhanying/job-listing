ecc81c4d700ff19dc98780cdc97ccc998dad4643
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-12 10:56:34 -0500

    add require_is_admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,13 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
+
+  def require_is_admin
+    if current_user.email != 'kaihe1989@gmail.com'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
 
   def show
     @job = Job.find(params[:id])
