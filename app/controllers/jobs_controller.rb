5a7afa3a362bc3777c499cc7fd34c99a6eaf18ac
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-12 15:52:36 -0500

    hidden job cannot be seen in index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false)
   end
 
   def show
