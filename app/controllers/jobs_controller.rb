24051d2ffcbe832c6b20556872ac9fee3f06d83a
youying0356 <youying0356@qq.com>
2017-01-12 23:31:50 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,8 +5,17 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
   end
+
+
 
   def create
     @job = Job.new(job_paramas)
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 namespace :admin do
   resources :jobs
 end
+  resources :jobs
   root 'jobs#index'
 end
