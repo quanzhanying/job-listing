ab8a0e4c4d96d9a0c13c4961f6a9f155c7e8eb48
zhongjie <zhongjie10@gmail.com>
2017-01-13 00:24:02 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'jobs#index'
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
 
   namespace :admin do
     resources :jobs
