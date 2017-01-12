fddee80e753a1c0350da70e627ed1219647ba5cf
jay.g <jkland927@hotmail.com>
2017-01-12 16:37:44 +0800

    implement job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  resources :jobs
+  root 'jobs#index'
 end
