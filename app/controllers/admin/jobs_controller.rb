7835c55005b88e15cbc8cfb62a24fbc3a4489b53
jay.g <jkland927@hotmail.com>
2017-01-12 19:06:48 +0800

    implement part2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,54 +1,57 @@
-class JobsController < ApplicationController
+class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-
-  def index
-    @jobs =Job.all
-  end
+  before_action :require_is_admin
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  def index
+    @jobs = Job.all
   end
 
   def new
     @job = Job.new
   end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
-
   def create
     @job = Job.new(job_params)
+
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
-      render  :new
+      render :new
     end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
   end
 
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path, notice: 'Update success.'
+      redirect_to admin_jobs_path
     else
       render :edit
     end
   end
 
-  def destroy
+  def destory
     @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to jobs_path, alert: "Job deleted."
+    @job.destory
+    redirect_to admin_jobs_path
   end
 
   def require_is_admin
-    if current_user.admin?
+    if !current_user.admin?
       flash[:alert]='You are not admin'
       redirect_to root_path
     end
   end
 
-private
+  private
+
   def job_params
     params.require(:job).permit(:title, :description)
   end
