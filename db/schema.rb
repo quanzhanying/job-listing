4c591a9b8f1990ce3c426f0ce00a01123e10f84d
lianghaibin <lianghaibin1688@gmail.com>
2017-01-13 14:13:42 +0800

    exam

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170111161607) do
 
-  create_table "jobs", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
