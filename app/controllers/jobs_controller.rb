6a9b3118e6250b1a52d172fa233c36e0cc3c8722
xiubinpan@gmail.com <xiubinpan@gmail.com>
2017-01-13 17:13:59 +0800

    implement wage part3

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -47,6 +47,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
