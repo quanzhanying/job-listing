2a74b7b3376ee0b96c2f9c721be2d602ae637b5f
tuheng <tuhruonral@hotmail.com>
2017-01-13 21:04:36 +0800

    step 6 prder by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,7 +11,14 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.order('created_at DESC')
+            end
   end
 
   def new
