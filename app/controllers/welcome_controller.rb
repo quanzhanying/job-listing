3e3638fdece7328aca28c420c6e6b8c19e36ccfb
Yammy <yammyxing@foxmail.com>
2017-01-13 16:12:32 +0800

    add some new to beautify root page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:notice] = "Good Night!"
+    # flash[:notice] = "Good Night!"
+    flash[:notice] = "每天都能有所收获，人生就是幸福的!"
   end
 end
