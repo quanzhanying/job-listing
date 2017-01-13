19988621314e58639eba1d0fb2eb74367757785e
yaoyizhi <email@yaoadou888>
2017-01-13 19:31:22 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Rails.application.routes.draw do
   namespace :admin do
   resources :jobs do
     member do
-      post :Publish
+      post :publish
       post :hide
     end
+
+    resources :resumes
     end
   end
   resources :jobs do
     resources :resumes
-  end 
+  end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
