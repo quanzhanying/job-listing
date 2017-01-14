f9d51aa2be3dacaeabc8979652020bf7a4307183
Cindy <cindyjiang88@163.com>
2017-01-14 12:47:19 +0800

    implement job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+
+  namespace :admin do
+  resources :jobs
+end
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-root 'welcome#index'
+resources :jobs
+root 'jobs#index'
 end
