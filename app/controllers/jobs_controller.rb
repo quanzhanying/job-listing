bd3c4df651fdb7edb0037d6ba388fe505e219dbc
June <yuanzhongyan@gmail.com>
2017-01-14 22:34:02 +0800

    edit/update/delete (CRUD done)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
   def index
     @jobs = Job.all
   end
@@ -6,4 +8,47 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+
+    redirect_to jobs_path, alert: `Job deleted`
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs
-  root 'welcome#index'
+  root 'jobs#index'
 end
