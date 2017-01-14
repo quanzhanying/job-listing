399ed95861ff44b3621137456e04ba62544488c4
tuheng <tuhruonral@hotmail.com>
2017-01-14 11:48:05 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
 
   resources :jobs do
     resources :resumes
   end
-  
+
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
