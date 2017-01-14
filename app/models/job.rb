d418175d79006b84896bca620c2f1d5c90456c78
kaihe1989@gmail.com <hekai@HekaitekiMacBook-Pro.local>
2017-01-13 19:24:52 -0500

    link resume to User/Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,16 @@
 class Job < ApplicationRecord
+
+  has_many :resumes
+
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
-  
+
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+
+
 
   def publish!
     self.is_hidden = false
