edc037d08c5aa1429c04d308e687a61a01d5b541
Zoe-LuXIA <kilulu.register@gmail.com>
2017-01-16 01:16:53 +0800

    add sorting function and button

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,15 @@ class JobsController < ApplicationController
 	before_action :require_is_admin, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
-		@jobs = Job.where(:is_hidden => false).order("created_at DESC")
+		@jobs = case params[:order]
+		when 'by_lower_bound'
+			Job.where(:is_hidden => false).order("wage_lower_bound DESC")
+		when 'by_upper_bound'
+			Job.where(:is_hidden => false).order("wage_upper_bound DESC")
+		else
+			Job.where(:is_hidden => false).order("created_at DESC")
+		end
+
 	end
 
 	def show
