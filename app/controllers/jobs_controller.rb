4ae72b845fe09e0a824448fbc16d1ce9cc24e6ad
June <yuanzhongyan@gmail.com>
2017-01-15 20:50:29 +0800

    新增薪资上限、薪资下限、联络 Email

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -48,7 +48,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 end
