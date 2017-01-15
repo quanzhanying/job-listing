70d6d6696ae81f1c4e9098a76a2407cf07e06646
shijunfeng <johnshiluck@gmail.comgit>
2017-01-16 00:26:00 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
 
   resources :jobs do
     resources :resumes
   end
-  
+
   root 'jobs#index'
 end
