e6ad94bcfd28302b14f3eaa14bc18adc91f31dd0
“将叫我将军” <d2194499406@gmail.com>
2017-01-15 17:40:08 +0800

    step6 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -11,6 +11,8 @@ class Job < ApplicationRecord
     self.save
   end
 
+  has_many :resumes
+
   validates :title, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_upper_bound, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :jobs
+
   namespace :admin do
     resources :jobs do
       member do
@@ -10,5 +9,10 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :jobs do
+    resources :resumes
+  end
+
   root 'jobs#index'
 end
