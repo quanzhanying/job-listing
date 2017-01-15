f99ff678414546b74d95d27dcb740a6bf411bd58
shijunfeng <johnshiluck@gmail.comgit>
2017-01-15 22:25:59 +0800

    step 7.2 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
-  root 'jobs#index'
-
   namespace :admin do
     resources :jobs do
       member do
@@ -12,4 +9,10 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :jobs do
+    resources :resumes
+  end
+  
+  root 'jobs#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114165519) do
+ActiveRecord::Schema.define(version: 20170115132739) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20170114165519) do
     t.string   "contact_email"
     t.string   "contact_phone"
     t.boolean  "is_hidden",        default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.integer  "job_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
