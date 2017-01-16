a41ca24ed74ade85f8af9cc4ce4b1c031614988f
mali <mailfullstack@gmail.com>
2017-01-16 14:01:21 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources:jobs do
+    resources :resumes
   end
 
   namespace :admin do
