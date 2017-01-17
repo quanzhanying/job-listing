be2f48e38cdc875b5143d7db6450463bf9486b22
mayue <mayue@mayuedeMacBook-Air.local>
2017-01-17 19:14:00 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -48,7 +48,7 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 end
