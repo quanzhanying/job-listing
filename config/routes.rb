408ca4687c76697f90e08e4d2bb1bc9ccbcf50c9
zhongjie <zhongjie10@gmail.com>
2017-01-17 23:53:30 +0800

    implement more

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   resources :jobs
 
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
   #root 'welcome#index'
 
