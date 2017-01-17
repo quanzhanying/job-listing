f61a6b4c6d3f507593adc7832ca60d7ac28bc3dc
mayue <mayue@mayuedeMacBook-Air.local>
2017-01-17 19:44:24 +0800

    implement wage part

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
 
+
+        
   def admin?
-    is_admin
-  end
+     is_admin
+   end
 
 end
