01548ccac6fa83020d57e5b3e5b0fc9a5f06f1a9
xyy601chenyan <xyy545688506@gmail.com>
2017-01-18 22:23:25 +0800

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts '设置管理员，生成隐藏job和显示job'
+create_account = User.create([email: 'xyy123@gmail.com', password: 'xyy123',
+                 password_confirmation: 'xyy123', is_admin: 'true'])
+puts '管理员设置成果'
+
+job_info = ['developer',
+            'teacher',
+            'editor',
+            'engineer',
+            'dancer']
+
+create_jobs = for i in 1..10 do
+             Job.create!([title: job_info[rand(job_info.length)],description: 'publish job',
+             wage_lower_bound: rand(40..70)*800, wage_upper_bound: rand(40..70)*1000 , is_hidden: 'false'])
+end
+puts '10 publish jobs created'
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: job_info[rand(job_info.length)], description: "hidden job", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'true'])
+end
+puts '10 hidden jobs created'
