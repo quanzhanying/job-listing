8a43ee94c872a2322dfaec07a38393684fe60391
xiubinpan@gmail.com <xiubinpan@gmail.com>
2017-01-19 19:51:47 +0800

    implement Admin Panel

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
 
   def show
     @job = Job.find(params[:id])
