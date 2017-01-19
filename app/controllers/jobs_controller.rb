b19f522412274a393e3e269fdbbce65f6f6bcc9d
xiubinpan@gmail.com <xiubinpan@gmail.com>
2017-01-19 19:23:35 +0800

    implement jobs#edit update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,6 +21,19 @@ class JobsController < ApplicationController
     end
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def job_params
