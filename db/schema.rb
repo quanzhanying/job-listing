bc48c3a9e559cab346c60f294590574ee8219562
maggie <maggie20161225@gmail.com>
2017-01-19 16:57:24 +0800

    show action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119035745) do
+ActiveRecord::Schema.define(version: 20170119085241) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
