5328de2c9cac6cf484e90d894d4ff0b7295bcefb
maggie <maggie20161225@gmail.com>
2017-01-20 12:46:09 +0800

    admin dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :admin do
-   resources :jobs
-end
+
    resources :jobs
    root 'jobs#index'
+   namespace :admin do
+    resources :jobs
+ end
 end
