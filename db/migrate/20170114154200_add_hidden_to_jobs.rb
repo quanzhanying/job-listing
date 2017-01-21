024d6ee45567814fd40061a009e9544593303d79
Zoe-LuXIA <kilulu.register@gmail.com>
2017-01-21 16:53:57 +0800

    reset the files

--- db/migrate/20170114154200_add_hidden_to_jobs.rb
+++ db/migrate/20170114154200_add_hidden_to_jobs.rb
@@ -1,5 +1,5 @@
 class AddHiddenToJobs < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :is_hidden, :boolean, default: true
+    add_column :jobs, :is_hidden, :boolean, default: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(version: 20170114154200) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "user_id"
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
-    t.boolean  "is_hidden",        default: false
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20170114154200) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
-    t.boolean  "is_hidden",              default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
