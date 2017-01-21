98e4c55efdbd259833ec7b6b1ed4cc5ee7960fd9
youying0356 <youying0356@qq.com>
2017-01-21 22:46:01 +0800

    admin can not see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ namespace :admin do
       post :publish
       post :hide
     end
+    resources :resumes
   end
 end
 
@@ -14,5 +15,6 @@ end
     resources :resumes
   end
 
+  
   root 'welcome#index'
 end
