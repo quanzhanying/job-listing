b2bde022ff0a5c33745aaecbbd46dac63046efad
RsMacAir <51xiuxian@sina.com>
2017-01-21 08:14:23 +0800

    replace route path to jobs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :jobs
-root 'welcome#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'jobs#index'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
