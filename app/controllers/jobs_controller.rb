cd970031da4e3783b0fbed54cf7560907c5c6f43
RsMacAir <51xiuxian@sina.com>
2017-01-22 06:49:26 +0800

    implement step5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,11 @@ before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end 
   end
 
   def index
