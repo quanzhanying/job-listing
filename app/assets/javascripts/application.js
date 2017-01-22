3e4acb92e848388a1eee5e4d49f9d682bf93ed69
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-22 20:56:01 +0800

    welcome beautify

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,15 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
+//= require bootstrap-sprockets
 //= require_tree .
+
+
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,3 @@
 class WelcomeController < ApplicationController
-  def index
-    flash[:notice] = "欢迎！！"
-  end
+
 end
