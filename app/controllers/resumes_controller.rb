11d96010c119f0df9974752a623030ad6c6bb010
maggie <maggie20161225@gmail.com>
2017-01-23 12:57:48 +0800

    ignore uploads

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,28 +1,32 @@
 class ResumesController < ApplicationController
-  before_action :authenticate_user!
+    before_action :authenticate_user!
 
-  def new
-    @job = Job.find(params[:job_id])
-    @resume = Resume.new
-  end
-
-  def create
-    @job = Job.find(params[:job_id])
-    @resume = Resume.new(resume_params)
-    @resume.job = @job
-    @resume.user = current_user
-
-    if @resume.save
-      flash[:notice] = "成功提交履历"
-      redirect_to job_path(@job)
-    else
-      render :new
+    def new
+        @job = Job.find(params[:job_id])
+        @resume = Resume.new
     end
-  end
 
-  private
+    def create
+        @job = Job.find(params[:job_id])
+        @resume = Resume.new(resume_params)
+        @resume.job = @job
+        @resume.user = current_user
 
-  def resume_params
-    params.require(:resume).permit(:content)
-  end
+        if @resume.save
+            flash[:notice] = '成功提交履历'
+            redirect_to job_path(@job)
+        else
+            render :new
+        end
+    end
+
+    def resume_params
+        params.require(:resume).permit(:content, :attachment)
+    end
+
+    private
+
+    def resume_params
+        params.require(:resume).permit(:content)
+    end
 end
