0a8f1d4ef69e80ea13aaa1d6e24f468066a0a58d
maggie <maggie20161225@gmail.com>
2017-01-23 13:33:27 +0800

    admin can see resume list

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,68 +1,68 @@
 class Admin::JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
     before_action :require_is_admin
-    layout "admin"
+    layout 'admin'
 
-  def show
-    @job = Job.find(params[:id])
-  end
-
-  def index
-    @jobs = Job.all
-  end
-
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = Job.new(job_params)
-
-    if @job.save
-      redirect_to admin_jobs_path
-    else
-      render :new
+    def show
+        @job = Job.find(params[:id])
     end
-  end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to admin_jobs_path
-    else
-      render :edit
+    def index
+        @jobs = Job.all
     end
-  end
 
-  def destroy
-    @job = Job.find(params[:id])
+    def new
+        @job = Job.new
+    end
 
-    @job.destroy
+    def create
+        @job = Job.new(job_params)
 
-    redirect_to admin_jobs_path
-  end
+        if @job.save
+            redirect_to admin_jobs_path
+        else
+            render :new
+        end
+    end
 
-  def publish
-   @job = Job.find(params[:id])
-   @job.publish!
-   redirect_to :back
-  end
+    def edit
+        @job = Job.find(params[:id])
+    end
 
-  def hide
-  @job = Job.find(params[:id])
+    def update
+        @job = Job.find(params[:id])
+        if @job.update(job_params)
+            redirect_to admin_jobs_path
+        else
+            render :edit
+        end
+    end
 
-  @job.hide!
+    def destroy
+        @job = Job.find(params[:id])
 
-  redirect_to :back
-  end
+        @job.destroy
 
-  private
+        redirect_to admin_jobs_path
+    end
 
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
-  end
+    def publish
+        @job = Job.find(params[:id])
+        @job.publish!
+        redirect_to :back
+    end
+
+    def hide
+        @job = Job.find(params[:id])
+
+        @job.hide!
+
+        redirect_to :back
+    end
+
+    private
+
+    def job_params
+        params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+    end
 end
