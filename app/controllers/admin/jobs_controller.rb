8b7643abc742bb2e9c294e475b257ea30a411ace
RsMacAir <51xiuxian@sina.com>
2017-01-26 16:48:21 +0800

    add admin limit

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
 before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+before_action :require_is_admin
 
 def index
   @jobs = Job.all
