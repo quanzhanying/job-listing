116419293c5e2c2e68909510524c54d4cc55ea2e
qizhengxin <qizhengxin721@gmail.com>
2017-01-26 17:13:46 +0800

    haha

---
+++ app/controllers/admin/works_controller.rb
@@ -0,0 +1,52 @@
+class Admin::WorksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :update, :create, :edit, :destroy]
+  before_action :require_is_admin
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to admin_works_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+  end
+
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(work_params)
+      redirect_to admin_works_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy
+    redirect_to admin_works_path
+  end
+
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :works
+
+  namespace :admin do
+    resources :works
+  end
   root 'works#index'
 end
