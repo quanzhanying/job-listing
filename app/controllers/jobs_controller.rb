d72e1193c7f7d512d1f9f5077e18d01ba597cb0e
kai <328458211@qq.com>
2017-01-27 13:26:53 +0800

    temporary save

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,13 @@
 class JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
   def index
     @jobs = Job.all
   end
+
+
+  def new
+    @job = Job.new
+  end
+
 end
