cd6810bdeb7c7552c1f80f2276dd19f7c05b5a9d
lichen <lichen19844@gmail.com>
2017-01-27 16:01:08 -0800

    step7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,6 +7,8 @@ class Job < ApplicationRecord
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
 
+  has_many :resumes
+
   def publish!
     self.is_hidden = false
     self.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jobs
 
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
-      end      
+      end
     end
   end
+
+  resources :jobs do
+    resources :resumes
+  end
+
 
   root 'jobs#index'
 end
