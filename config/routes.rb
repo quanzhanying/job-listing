eb3ee7609ad74b9e97043b35918783afc2f93d97
kai <328458211@qq.com>
2017-01-28 23:43:47 +0800

    send resume form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :jobs
 
   namespace :admin do
     resources :jobs do
@@ -11,5 +10,10 @@ Rails.application.routes.draw do
         end
     end
   end
+
+  resources :jobs do
+      resources :resumes
+  end
+
   root 'jobs#index'
 end
