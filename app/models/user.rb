c3e7155dd131cf2805147bcaa4c60b44974cad6b
kai <328458211@qq.com>
2017-01-28 15:40:44 +0800

    temporary stop step 6 because of rails c bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+#  def admin?
+#      is_admin
+#  end
+#  因为rails c 有bug 暂时跳过这一步 "Step 6: 把 admin? 的条件改为由资料库判断"
   def admin?
-      is_admin
+    email == 'lilei@sina.com'
   end
 end
--- db/migrate/20170128033627_add_is_admin_to_user.rb
+++ db/migrate/20170128033627_add_is_admin_to_user.rb
@@ -1,5 +1,6 @@
 class AddIsAdminToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :is_admin, :boolean, default: false
-  end
+#  def change
+#    add_column :users, :is_admin, :boolean, default: false
+#  end
+# 因为rails c 有bug 暂时跳过这一步
 end
