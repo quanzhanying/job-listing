bad2deb2f4ee4192dfe0cc1c7e8940c1eff7eb9c
RsMacAir <51xiuxian@sina.com>
2017-01-29 10:19:03 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ namespace :admin do
       post :public
       post :hide
     end
+    resources :resumes
   end
 end
   resources :jobs do
     resources :resumes
-  end 
+  end
 
   root 'jobs#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
