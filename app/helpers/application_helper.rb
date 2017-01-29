f447612f617c419048ad31130dfd898d345c18eb
lichen <lichen19844@gmail.com>
2017-01-28 22:36:44 -0800

    add a landing box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+      @resource ||= User.new
+  end
+
+  def resource_class
+      User
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+  end
 end
