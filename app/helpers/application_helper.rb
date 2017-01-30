ec4176c494f34da5b41f6267c6363c2372a8cb53
qizhengxin <qizhengxin721@gmail.com>
2017-01-30 13:47:48 +0800

    hahha

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,21 @@ module ApplicationHelper
   def user_facing_flashes
     flash.to_hash.slice "alert", "notice", "warning"
   end
+
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+  end
 end
