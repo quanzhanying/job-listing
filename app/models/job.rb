b42878ce90c173e1cbe4b5656be35847f576622c
“将叫我将军” <d2194499406@gmail.com>
2017-01-31 00:21:52 +0800

    can category but git push

--- app/models/job.rb
+++ app/models/job.rb
@@ -11,13 +11,14 @@ class Job < ApplicationRecord
     self.save
   end
 
-  belongs_to :apps
+  belongs_to :category
   has_many :resumes
 
   validates :title, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0 }
+  validates :category_id, :presence => true, allow_nil: false
 
   def self.search(search)
     where("title LIKE ?", "%#{search}%").or(where("description LIKE ?", "%#{search}%"))
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,25 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ :name => 'Star Wars' }, { :name => 'Lord of the Rings' }])
+#   Character.create{ :name => 'Luke', movie: movies.first)
 
 puts "这个seeds文件可以自动建立一个admin账号，并且创建10个public jobs,以及10各hidden jobs"
 
 create_account = User.create([email: 'd280775064@126.com', password:'123456', password_confirmation: '123456', is_admin: 'true'])
 puts "Admin account created."
+
+categories = Category.create([
+{ :name => '财务|人力资源|行政'},
+{ :name => '互联网|IT|通信'},
+{ :name => '房产|建筑|物业管理'},
+{ :name => '金融'},
+{ :name => '采购|贸易|交通|物流'},
+{ :name => '生产制造'},
+{ :name => '咨询|法律|教育|翻译'},
+{ :name => '服务业'},
+{ :name => '兼职|实习|社工|其他'}
+])
 
 create_job = for i in 1..10 do
   Job.create!([title: "Job no.#{i}", description: "这是用seeds文件建立的第 #{i} 个公开的工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
