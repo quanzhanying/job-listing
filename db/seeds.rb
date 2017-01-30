a3febd7289c52aec7034fc6eb065a949a587ed28
qizhengxin <qizhengxin721@gmail.com>
2017-01-30 14:13:28 +0800

    jij9

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+create_account = User.create([email: '1e@1', password: '111111', password_confirmation: '111111', is_admin: 'true'])
+puts "Admin account created."
+
+create_jos = for i in 1..10 do
+  Work.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+end
+puts "10 Public jobs created."
+
+create_jos = for i in 1..10 do
+  Work.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+end
+puts "10 Hidden jobs created."
