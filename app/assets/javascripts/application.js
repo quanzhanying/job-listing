be0276b3f441e501e0232555d40fed8c6a459811
brooks <13764390725@139.com>
2017-01-31 13:20:00 +0800

    finished pop-up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //=require bootstrap/alert
-#//=require bootstrap/dropdown
+//=require bootstrap/dropdown
+//=require bootstrap/modal
 //= require_tree .
