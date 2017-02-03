8a245bc861304e8763e0df9dff07f9dd64e86bef
xiubinpan@gmail.com <xiubinpan@gmail.com>
2017-02-03 14:24:19 +0800

    add sidebar to admin panel

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  layout "admin"
 
   def show
     @job = Job.find(params[:id])
