70898a9908f34c6767c571e624a7d1217c7d5974
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-02-05 17:03:22 +0800

    implement jobs#destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -33,6 +33,12 @@ class JobsController < ApplicationController
     end
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert:"Job Deleted"
+  end
+
   private
 
   def job_params
