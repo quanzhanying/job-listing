bb5f42e5c41318ef14c1a0bd6d548d42b97e1adc
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-02-07 12:32:10 +0800

    db:add_is_hidden_to_job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false)
   end
 
   def new
