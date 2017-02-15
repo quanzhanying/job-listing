8058b7cee7ec9167229bb0fc62da24d49f44a9b0
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-02-15 13:52:43 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -20,6 +20,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job is already archieved."
+      redirect_to root_path
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   resources :jobs
 
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
 
   root 'jobs#index'
