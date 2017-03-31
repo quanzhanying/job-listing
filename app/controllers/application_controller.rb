dfba6b7bdacedaa916c74d605f73819beda510ce
liuzhetz007 <liuzhetz007@126.com>
2017-03-31 08:36:02 +0800

    user can login/logout/singnup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  def index
+    flash[:notice] = "早安！你好！"
+  end
 end
--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,10 +1,11 @@
-modul FlashesHelper
-  FLSASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
+module FlashesHelper
+  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
   end
+
   def user_facing_flashes
-    flashe.to_hash.slice "alert", "notice","warning"
+    flash.to_hash.slice "alert", "notice","warning"
   end
 end
