116cc4f1a7f687f59e7451c244dd6541d11888ce
eagle111 <18645125206@sina.cn>
2017-04-05 18:56:20 +0800

    implement part 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def show
     @job=Job.find(params[:id])
   end
