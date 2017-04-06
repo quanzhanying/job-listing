35b68d57702d7e520eec81a28d3821c3f9af8d72
chenwy <chenwy323@163.com>
2017-04-06 22:50:39 +0800

    implemnt part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -3,16 +3,50 @@ class Admin::JobsController < ApplicationController
   before_action :require_is_admin
 
   def index
+    @jobs = Job.all.recent
+  end
 
+  def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_param)
+
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_param)
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path
   end
 
   private
 
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = "You are not admin"
-      redirect_to root_path
-    end
+  def job_param
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 end
