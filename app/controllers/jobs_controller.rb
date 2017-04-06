04eca96d168350c6a41ca1c915d84af7b3ce7885
chenwy <chenwy323@163.com>
2017-04-06 19:22:53 +0800

    implement jobs add/eidt/delete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,5 +4,42 @@ class JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_param)
+    @job.save
+
+    redirect_to jobs_path
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_param)
+
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_param
+    params.require(:job).permit(:title, :description)
+  end
 
 end
