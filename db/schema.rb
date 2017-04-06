21c7d4fe2c1c231cd57b2d8b9701d0644a458535
jiaowagn <wangjiaohere@gmail.com>
2017-04-07 00:30:05 +0800

    create jobs index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406140247) do
+ActiveRecord::Schema.define(version: 20170406153100) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
