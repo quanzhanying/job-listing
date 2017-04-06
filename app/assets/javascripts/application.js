eb29d2e26dbb5db30bd0f67c2a49b9af98f2a99c
peck liu <xiaoyang20170204@gmail.com>
2017-04-06 10:18:58 +0800

    crud implements done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap/alert
+//= require bootstrap/dropdown
 //= require_tree .
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,38 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobs_params)
+    @job.save
+    redirect_to jobs_path,notice:"Job Added!"
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path,alert:"Job Deleted!"
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(jobs_params)
+    redirect_to jobs_path,alert:"Job Updated!"
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+  private
+  def jobs_params
+    params.require(:job).permit(:title,:description)
+  end
 end
