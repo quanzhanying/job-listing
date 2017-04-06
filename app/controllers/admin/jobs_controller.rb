1891d7d5e5e30cbc07505a654a30fc04686dbe96
zhedian <cooldeer@126.com>
2017-04-07 01:42:10 +0800

    add is hidden

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -6,5 +6,24 @@ class Admin::JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params1)
+      redirect_to admin_jobs_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def job_params1
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
+  end
+
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
   before_action :find_job, only: [:edit, :update, :destroy, :show]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
@@ -49,6 +49,6 @@ class JobsController < ApplicationController
   end
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 end
