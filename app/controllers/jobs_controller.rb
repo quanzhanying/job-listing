97eb5f59ea1504beee7b90b7b83cc2c00160e57f
田汀 <imtinge@gmail.com>
2017-04-06 12:00:26 +0800

    complete step 1

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+  # before_action :authenticate_user!, only: [:index]
   def index
     flash[:warning] = "warning"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'jobs/index'
+  devise_for :users
+  resources :jobs
+  root 'jobs#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
