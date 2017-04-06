76b2d933cd23cb994b3d92f4b7e2127f064e09b2
yhk <yhhklndexnug@gmail.com>
2017-04-06 21:03:18 +0800

    add bootstrap flash function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "加油 努力干"
+    flash[:warning]= "jwyb nuligj"
+    flash[:alert] = "jy ujg"
+  end
 end
--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,8 +1,8 @@
 module FlashesHelper
-  FLASH_FLASHES = { alert: "danger", notice: "success",warning: "warning"}.freeze
+  FLASH_CLASSES = { alert: "danger", notice: "success",warning: "warning"}.freeze
 
   def flash_class(key)
-    FLASH_CLASSES.fetch key.to_sym,key
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
