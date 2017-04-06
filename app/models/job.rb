c192af3c4012968093028c0fddddadaa51487182
ken <aken@matoMac-mini-3.local>
2017-04-06 18:14:53 +0800

    implement add is admin to user

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :jobs
-  root 'jobs#index'
   devise_for :users
+  resources :jobs
+
+  namespace :admin do
+    resources :jobs
+  end
+
+  root 'jobs#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
