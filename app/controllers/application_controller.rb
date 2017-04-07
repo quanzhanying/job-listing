be0fe8ee7c49df3d917cb9485414fe0d917d4a01
lotus <lvdeshi2011@163.com>
2017-04-07 16:17:39 +0800

    implement wage part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def require_id_admin
+
+  def require_is_admin
     if !current_user.admin?
       redirect_to root_path, alert: "You aren't admin"
     end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+  validates :wage_lower_bound, presence: true
+  vaildates :wage_lower_bound,numericality: { greater-than： 0}
+  validates :wage_upper_bound, presence: true
+  validates :contact_email, presence: true
 end
