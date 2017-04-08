8eb1ba9dbfceeab27c4b4bc8c52e23a269d4abe2
AlexPan <ppgod@live.cn>
2017-04-08 16:52:51 +0800

    implement is_hidden to job and order by to root page jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408080008) do
+ActiveRecord::Schema.define(version: 20170408084244) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "wage_lower_bound"
     t.integer  "wage_upper_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: false
   end
 
   create_table "users", force: :cascade do |t|
