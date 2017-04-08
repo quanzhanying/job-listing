08520e38de87091e2ac6c8d262ba8c789e59991f
Lucia <learningleadtorebirth@gmail.com>
2017-04-08 14:09:52 +0800

    add validation to title

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,8 +10,11 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-    redirect_to jobs_path
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -24,8 +27,11 @@ class JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
-    @job.update(job_params)
-    redirect_to jobs_path, notice: "Update Success"
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
