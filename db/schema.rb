2d86681e0f5d16eb6bcb55b35851bdc076988140
Lucia <learningleadtorebirth@gmail.com>
2017-04-08 17:19:40 +0800

    add is_hidden to job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408083333) do
+ActiveRecord::Schema.define(version: 20170408091044) do
 
   create_table "job_relationships", force: :cascade do |t|
     t.integer  "job_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170408083333) do
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden"
   end
 
   create_table "users", force: :cascade do |t|
