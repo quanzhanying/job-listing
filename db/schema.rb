b3883cce6b2d9c0d4c595dc12d07ab3eaf74743c
anndoko <handcuffanndo@gmail.com>
2017-04-08 15:53:48 +0800

    implement part4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408064853) do
+ActiveRecord::Schema.define(version: 20170408074223) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170408064853) do
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden"
   end
 
   create_table "users", force: :cascade do |t|
