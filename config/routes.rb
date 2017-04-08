3a23433909bf242a84a7e5f256f628e34bf4921f
liujia <ericrunner123456@yahoo.com>
2017-04-08 21:47:22 +0800

    add admin/jobs controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'jobs#index'
+  namespace :admin do
+    resources :jobs
+  end 
 end
