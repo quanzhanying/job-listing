00f0cad5ca0437e3a40d76ef87a13a5c95452e6c
Jony <xiayang1012@gmail.com>
2017-04-08 17:26:47 +1000

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -41,11 +41,11 @@ class JobsController < ApplicationController
     redirect_to jobs_path
   end
 
-  
+
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
   end
 
 
