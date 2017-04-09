9f34be7903b3041cf319f3f88922da3fea910d68
郭姝彤 <Lynsmiling@outlook.com>
2017-04-09 14:34:11 +0800

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  resources :jobs
+  root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
