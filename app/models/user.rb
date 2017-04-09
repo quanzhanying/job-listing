4d2a1543661eb7245b21687c79fcf08e7ab39b8a
zhaoxinxi <90zhaoxinxi@gmail.com>
2017-04-09 11:08:04 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def admin?
+    is_admin
+  end
 end
