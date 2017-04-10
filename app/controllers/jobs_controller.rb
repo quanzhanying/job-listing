d0bd72dcdb4ce805714e99fa0a289c0e288d1803
beexpectedone <wqyfighting@163.com>
2017-04-11 00:56:52 +0800

    implement jobs#show

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,27 @@
+class JobsController < ApplicationController
+	def index
+	   @jobs = Job.all
+	end
+
+	def new
+		@job = Job.new
+	end		
+
+	def create
+		@job = Job.new(job_params)
+		@job.save
+
+		redirect_to jobs_path
+	end
+
+	def show
+		@job = Job.find(params[:id])
+	end
+
+
+	private
+
+	def job_params
+		params.require(:job).permit(:title, :description)
+	end	
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410152641) do
+ActiveRecord::Schema.define(version: 20170410162830) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
