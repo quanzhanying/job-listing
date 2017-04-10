dc192bf6d288d474a02a77911bd08056f63292be
AlexPan <ppgod@live.cn>
2017-04-10 23:58:54 +0800

    implement order with wage_lower_bound and wage_upper_bound

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,14 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.where(is_hidden: false).order("created_at DESC")
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.where(is_hidden: false).order("wage_lower_bound DESC").paginate(page: params[:page], per_page: 10)
+            when 'by_upper_bound'
+              Job.where(is_hidden: false).order("wage_upper_bound DESC").paginate(page: params[:page], per_page: 10)
+            else
+              Job.where(is_hidden: false).order("created_at DESC").paginate(page: params[:page], per_page: 10)
+            end
   end
 
   def new
