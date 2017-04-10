19b6a2bb20ac8f24a8e8dfd24642e524504d449f
wuzhao <liwuzhao97@gmail.com>
2017-04-10 16:17:21 +0800

    用bootsrap装饰navbar

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,7 +1,7 @@
 module JobsHelper
   def render_job_status(job)
       if job.is_hidden
-        content_tag(:span, "", :class => "fa fa-lock")
+        content_tag(:span, "", :class => "fa fa-lock",)
       else
         content_tag(:span, "", :class => "fa fa-globe")
       end
