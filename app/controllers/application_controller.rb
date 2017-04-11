43b21030ed9f726ebce7b079ee4bd15653744905
emilesu <emilesu@qq.com>
2017-04-11 23:04:59 +0800

    implement step 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def require_is_admin
-    unless current_user.admin?
-      flash[:alert] = "你不是管理员！"
+    if !current_user.admin?
+      flash[:alert] = 'you are not admin'
       redirect_to root_path
     end
   end
