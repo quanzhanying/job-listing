893958266e0153a2acd259de83073e6bfe01ce8b
guochunri <guochunri@gmail.com>
2017-04-12 01:20:16 +0800

    move form to partial

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -42,6 +42,6 @@ class JobsController < ApplicationController
 
 private
   def job_params
-    params.require(:job).permit(:title, :desciption)
+    params.require(:job).permit(:title, :description)
   end
 end
