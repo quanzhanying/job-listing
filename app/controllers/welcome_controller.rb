31d7233d0c8d0ea496435609c47fca4cd63b516a
Eric <zeric163@gmail.com>
2017-04-12 06:15:15 +0800

    user can login/logout/signup

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+
+  def index
+    flash[:notice] = "早安！你好！"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
 root 'welcome#index'
+
 
 end
