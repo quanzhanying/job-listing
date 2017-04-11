80e2bbcaa8aa9bc7b7538993830ee19dd7649cb2
donker008 <richdonker@qq.com>
2017-04-11 19:20:00 +0800

    bug fix for require_is_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def require_is_admin
-    unless  current_user.isAdmin?
+    unless  current_user.isAdmin
        flash[:alert] = "You are not administrator"
        redirect_to root_path
     end
