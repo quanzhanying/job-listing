8e67ca4b6900c41b7cd2d33d215394a596439b3c
sfxg27@gmail.com <feixiguang@feixiguangdeMacBook-Pro.local>
2017-04-12 23:35:41 +0800

    implement part4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
 
 
   def require_is_admin
-    if current_user.admin?
-      flash[:alert] = "You are not admin"
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
   end
-  
+
 end
