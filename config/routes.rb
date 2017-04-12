e4d40430bd124950e2e77053a7cdb1a7647d4b4f
josephlambert <478911359@qq.com>
2017-04-12 11:14:45 +0800

    add navbar footer welcome

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :jobs
+
+    root 'welcome#index'
 end
