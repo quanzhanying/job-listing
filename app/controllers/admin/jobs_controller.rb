b854e1ee738ef991d65692445ffc973e032bc283
陈帅 <rockychen7@gmail.com>
2017-04-12 15:47:38 +0800

    move require_is_admin_to_application_controller

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -42,13 +42,6 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
-  def require_is_admin
-    if current_user.email != 'xdite@growth.school'
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
-  end
-
   private
 
   def job_params
