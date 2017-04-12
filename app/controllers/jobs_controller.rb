161bdd12df73fb90b93ee3495ca5ef49fc164b4b
陈帅 <rockychen7@gmail.com>
2017-04-12 12:21:37 +0800

    implement jobs#edit,update,destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,7 +12,7 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
-  def created
+  def create
     @job = Job.new(job_params)
 
     if @job.save
@@ -20,6 +20,27 @@ class JobsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+
+    redirect_to jobs_path
   end
 
   private
