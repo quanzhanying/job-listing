d1510a72521a2ed81cac24cb5bba3cfa76894343
longmei2017@gmail.com <ankon@ankon-mac.local>
2017-04-13 10:59:58 +0800

    5.3 step 3-3

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,18 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy, :index]
+  before_action :require_is_admin
+
+  def index
+
+  end
+
+  private
+
+def require_is_admin
+  if !current_user.admin?
+    flash[:alert] = "You are not admin"
+    redirect_to root_path
+  end
+end
+
+end
