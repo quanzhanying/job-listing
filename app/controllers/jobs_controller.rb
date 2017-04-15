439abe93b8fe1560a9b1f72fbf01f3b4cf8056a7
Karliao <liaodunxia@gmail.com>
2017-04-15 10:13:05 +0800

    implement part2

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,56 @@
+class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_job_and_check_permission, only: [:edit, :update, :destroy]
+
+  def index
+    @jobs = Job.recent.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(params_job)
+    if @job.save
+      redirect_to jobs_path, notice: "Job created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @job.update(params_job)
+    if @job.save
+      redirect_to jobs_path, notice: "Update success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path, notice: "Job deleted"
+  end
+
+  private
+  def find_job_and_check_permission
+    @job = Job.find(params[:id])
+
+    # if current_user != @job.user
+    #   redirect_to root_path, alert: "You have no permission"
+    # end
+  end
+
+  def params_job
+    params.require(:job).permit(:title,:description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  #root 'welcome#index'
+
+  root 'jobs#index'
+
+  resources :jobs
+
+  namespace :admin do
+    resources :jobs
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
