24a684d9302aaa38ff1c5e1492beeb5b66b17ce6
郭姝彤 <Lynsmiling@outlook.com>
2017-04-16 10:48:38 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,5 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+  scope :published, -> { where(is_hidden: false) }
 end
