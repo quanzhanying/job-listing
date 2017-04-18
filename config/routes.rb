cc62f43241250deddbf5af0795ddef447324271f
郭姝彤 <Lynsmiling@outlook.com>
2017-04-18 17:40:27 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   resources :jobs do
     resources :resumes
   end
-  root 'jobs#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
