3dec544704aa9cf549ae76fe7e7421f4711542f8
Lucia <learningleadtorebirth@gmail.com>
2017-04-19 21:27:30 +0800

    welcome beauty

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :jobs do
     resources :resumes
   end
-  root 'jobs#index'
+  root 'welcome#index'
   namespace :admin do
     resources :jobs do
       member do
