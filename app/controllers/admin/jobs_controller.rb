5842e50ab1f338b539705180fdf9eac9230eb88a
tonyvicter <allan0212@hotamil.com>
2017-04-19 20:58:59 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -48,6 +48,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
