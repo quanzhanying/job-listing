f9e46f6c92b25250bb5039c6a053c4052408106e
zsmhy <zsm@example.com>
2017-04-20 23:42:54 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
+def admin?
+   is_admin
+end
