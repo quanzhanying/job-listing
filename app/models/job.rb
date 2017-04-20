56404bbb268ee7b23b13c6d944f1b3105b9389b9
tonyvicter <allan0212@hotamil.com>
2017-04-20 23:51:34 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -13,4 +13,7 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
+
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
 end
