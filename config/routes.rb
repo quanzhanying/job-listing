1a3c736ce7f59415cdfbfb8bfd5d10544d425dd0
sfxg27@gmail.com <feixiguang@feixiguangdeMacBook-Pro.local>
2017-04-22 14:05:56 +0800

    welcome or scss

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
       resources :resumes
     end
 
-  root 'jobs#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
