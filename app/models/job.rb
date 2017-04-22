891792e775e9ef4e80cd2e1dafe5c7252f3a7afa
qiuguixin <qguixin@qq.com>
2017-04-23 01:09:47 +0800

    step 7 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,9 +8,12 @@ class Job < ApplicationRecord
     self.is_hidden = false
     self.save
   end
-  
+
   def hide!
     self.is_hidden = true
     self.save
   end
+
+  scope :published, -> {where(is_hidden: false)}
+  scope :recent, -> {order('created_at DESC')}
 end
