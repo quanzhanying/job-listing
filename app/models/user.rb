5a508fe11e48599196155e9f41f483fcf986b712
qiuguixin <qguixin@qq.com>
2017-04-23 22:23:37 +0800

    upload resume

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :resumes
+
   def admin?
     is_admin
   end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
         end
       end
   end
-  resources :jobs
+
+  resources :jobs do
+    resources :resumes
+  end
+  
   root 'jobs#index'
 end
