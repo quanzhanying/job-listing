b36963fdb897f8ec0418afc9e42422772d4205f3
zhaoxinxi <90zhaoxinxi@gmail.com>
2017-04-23 11:54:38 +0800

    better welcome/index & admin/jobs/index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:notice]= "早安！你好！"
-    flash[:alert]= "早安！你好！"
-    flash[:warning]= "早安！你好！"
-  end  
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,5 @@ Rails.application.routes.draw do
     end
 
   end
-  root "jobs#index"
+  root "welcome#index"
 end
