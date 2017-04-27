14d153963c4f77739ccf1b91270c183831f0fef9
yangyixin <yix2730555@yahoo.com>
2017-04-27 19:57:59 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
 
    namespace :admin do
-   resources :jobs do
+   resources :jobs  do
      member do
        post :publish
        post :hide
+
      end
+     resources :resumes 
    end
  end
- 
+
  resources :jobs do
    resources :resumes
  end
