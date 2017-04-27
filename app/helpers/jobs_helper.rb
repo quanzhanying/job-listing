f3aee61d8cc6a54922341a721280d33555bddb85
Yopiluv <wwnchxz@hotmail.com>
2017-04-27 20:52:56 +0800

    implement step 5

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,9 +2,9 @@ module JobsHelper
 
   def render_job_status(job)
     if job.is_hidden
-      "(Hidden)"
+      content_tag(:span, "", :class => "fa fa-lock")
     else
-      "(Public)"
-    end    
+      content_tag(:span, "", :class => "fa fa-globe")
+    end
   end
 end
