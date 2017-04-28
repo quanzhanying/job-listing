13f867f882f5d35efb30ae600563dc3ef3cc8489
马筱静 <maxiaojing@maxiaojingdeMacBook-Pro.local>
2017-04-28 23:37:46 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -26,7 +26,8 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+
   end
 
   def edit
