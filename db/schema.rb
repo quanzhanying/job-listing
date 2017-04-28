33c3454e665800107a58bffafa4c6ebcfc74f8a3
emilesu <emilesu@qq.com>
2017-04-29 03:52:24 +0800

    debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428184223) do
+ActiveRecord::Schema.define(version: 20170428194957) do
 
   create_table "collects", force: :cascade do |t|
     t.integer  "job_id"
