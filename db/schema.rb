42ebcea2d80e04095deb8f812b40bbaef1d5b1dc
马筱静 <maxiaojing@maxiaojingdeMacBook-Pro.local>
2017-05-02 22:43:49 +0800

    step 7 upload resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501124101) do
+ActiveRecord::Schema.define(version: 20170501160532) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
