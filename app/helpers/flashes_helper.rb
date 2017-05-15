42bfee28866f14e8faf47321ed3c3458da12b53a
shijunfeng <johnshiluck@gmail.comgit>
2017-05-15 18:46:29 +0800

    add flash function

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -2,11 +2,11 @@ module FlashesHelper
   FLASH_CLASSES = { alert:'danger', notice:'success', warning:'warning'}.freeze
 
   def flash_class(key)
-    FLASH_CLASESS.fetch key.to_sym, key
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
     flash.to_hash.slice 'alert', 'notice', 'warning'
   end
-  
+
 end
