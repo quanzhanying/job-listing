906cbf985debeb1f56ca3c6c49ed892bd08b4f67
chenxiaoqing <pincha120@gmail.com>
2017-07-06 17:21:15 +0800

    implement job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+
+  resources :jobs
+  root 'jobs#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706081540) do
+ActiveRecord::Schema.define(version: 20170706083023) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
