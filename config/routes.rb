b6f3f2cf16e0a219051672dc9f960ea25dd6db32
zsmhy <zsm@example.com>
2017-07-07 02:24:10 +0800

    implement job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
 
-    devise_for :users
-    root 'welcome#index'  
+Rails.application.routes.draw do
+  devise_for :users
+
+   resources :jobs
+   root 'jobs#index'
 end
