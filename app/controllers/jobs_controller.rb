d829ba9cf5bd772708bc6340b989a2bcd9133662
zsmhy <zsm@example.com>
2017-07-07 22:41:28 +0800

    implement part 2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
+
   def show
     @job = Job.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :admin do
+    resources :jobs
+  end
+
    resources :jobs
    root 'jobs#index'
 end
