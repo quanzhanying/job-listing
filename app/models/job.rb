8e6287882f148306367e24f189bc4394387150ff
Cindy <lxx0314@126.com>
2017-07-21 12:17:58 +0800

    step7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,6 +14,8 @@ class Job < ApplicationRecord
     self.save
   end
 
+  has_many :resumes
+
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC')}
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
 
   namespace :admin do
     resources :jobs do
