ea4f2d8ea49e7d53722be312a9e651942e880410
高明真 <jingmosigui@sina.com>
2017-10-05 22:44:19 +0800

    建立了model、controller、index页，做出了step2的页面样式

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'jobs#index'
+  resources :jobs
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
