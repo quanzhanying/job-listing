ecfab407d5deac21c90be8a98fe460ce6991aec5
高明真 <jingmosigui@sina.com>
2017-10-06 15:27:19 +0800

    完成step3-1

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005131743) do
+ActiveRecord::Schema.define(version: 20171006022539) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
