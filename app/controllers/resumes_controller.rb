78c75896a9016a765f788a6e5f5cab586ff2e810
大圣 <yyf42336@gmail.com>
2018-11-21 14:50:04 +0800

    step 7 upload resume

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -7,23 +7,21 @@ class ResumesController < ApplicationController
   end
 
   def create
-    @movie = Movie.find(params[movie_id])
+    @movie = Movie.find(params[:movie_id])
     @resume = Resume.new(resume_params)
     @resume.movie = @movie
     @resume.user = current_user
+    if @resume.save
+      flash[:notice] = "成功提交影评"
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
   end
-
-  if @resume.save
-    flash[:notice] = "成功提交影评"
-    redirect_to movie_path(@movie)
-  else
-    render :new
-  end
-
-
 
   private
     def resume_params
-      params.require(:resume).permit(:content)
+      params.require(:resume).permit(:content, :attachment)
     end
+
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,7 +2,7 @@ class Resume < ApplicationRecord
   belongs_to :user
   belongs_to :movie
 
+  # mount_uploader :attachment, AttachmentUploader
   mount_uploader :attachment, AttachmentUploader
-  
   validates :content, presence: true
 end
