e6df63f04389c57de63ccea7d1627132078f6c28
BiFengJiang <irene2743506@gmail.com>
2019-04-07 22:46:04 +0800

    step 6 - order

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,17 @@ class JobsController < ApplicationController
   before_action :check_permission , only: [:edit, :update, :destroy]
 
   def index
-    @jobs = Job.where(:is_hidden => false).recent.paginate(:page => params[:page], :per_page => 10)
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+	      Job.where(:is_hidden => false).order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 10)
+	    when 'by_upper_bound'
+	      Job.where(:is_hidden => false).order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 10)
+	    else
+	      Job.where(:is_hidden => false).recent.paginate(:page => params[:page], :per_page => 10)
+	    end
+#    @jobs.paginate(:page => params[:page], :per_page => 10)
+#    Job.where(:is_hidden => false).recent.paginate(:page => params[:page], :per_page => 10)
+
   end
 
   def show
