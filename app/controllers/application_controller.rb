c5a13c38e51fb2e9f4e56cc9046fcc324298b42f
BiFengJiang <irene2743506@gmail.com>
2019-04-15 09:53:07 +0800

    some css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(user)
-    request.env['omniauth.origin'] || stored_location_for(user) || root_url
+    request.env['omniauth.origin'] || stored_location_for(user) || jobs_url
   end
 
   def after_sign_out_path_for(user)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def current_class?(test_path)
+        request.path == test_path ? 'active' : ''
+    end
 end
