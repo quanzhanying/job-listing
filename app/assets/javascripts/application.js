18a195115337e6f28d81d16f3be503d0f090c687
BiFengJiang <irene2743506@gmail.com>
2019-04-20 22:16:31 +0800

    login and signup modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require turbolinks
 //= require bootstrap/alert
 //= require bootstrap/dropdown
+//= require bootstrap/modal
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
     def current_class?(test_path)
         request.path == test_path ? 'active' : ''
     end
+
+    def resource_name
+        :user
+    end
+    
+    def resource
+        @resource ||= User.new
+    end
+    
+    def resource_class
+        User
+    end
+    
+    def devise_mapping
+        @devise_mapping ||= Devise.mappings[:user]
+    end
 end
