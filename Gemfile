51b6bf15b2e3e486b9c00b088509076cddc2c577
BiFengJiang <irene2743506@gmail.com>
2019-04-21 18:49:59 +0800

    use ransack to sort data

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'devise'
 gem 'will_paginate'
 gem 'font-awesome-rails'
 gem 'carrierwave'
+gem 'ransack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,14 +5,9 @@ class JobsController < ApplicationController
   before_action :check_permission , only: [:edit, :update, :destroy]
 
   def index
-    @jobs = case params[:order]
-      when 'by_lower_bound'
-	      Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 10)
-	    when 'by_upper_bound'
-	      Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 10)
-	    else
-	      Job.published.recent.paginate(:page => params[:page], :per_page => 10)
-	    end
+      @search = Job.ransack(params[:q])
+      @search.sorts = 'created_at desc' if @search.sorts.empty?
+      @jobs = @search.result.paginate(:page => params[:page], :per_page => 10)
 
   end
 
