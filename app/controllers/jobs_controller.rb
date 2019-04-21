d3759151b462cc2314c6e50e0e84b3d36ff96c59
BiFengJiang <irene2743506@gmail.com>
2019-04-21 10:38:39 +0800

    change order css

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,15 +6,13 @@ class JobsController < ApplicationController
 
   def index
     @jobs = case params[:order]
-            when 'by_lower_bound'
+      when 'by_lower_bound'
 	      Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 10)
 	    when 'by_upper_bound'
 	      Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 10)
 	    else
 	      Job.published.recent.paginate(:page => params[:page], :per_page => 10)
 	    end
-#    @jobs.paginate(:page => params[:page], :per_page => 10)
-#    Job.where(:is_hidden => false).recent.paginate(:page => params[:page], :per_page => 10)
 
   end
 
