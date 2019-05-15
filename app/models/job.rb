9f676239ef7d3fc63fe249435bed2473e24e3481
zee <kooo9671354@gmail.com>
2019-05-15 21:35:38 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,6 +9,7 @@ class Job < ApplicationRecord
     self.save
   end
 
+  has_many :resumes
 
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
 
   namespace :admin do
