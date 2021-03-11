d4faad5fa84cd1bb715c36037188ddcf8a7c859e
zhongjinye <610505041@qq.com>
2021-03-11 11:20:54 +0800

    implement job#show&create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,29 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @jobs = Job.all
   end
 
+  def show
+    @job = Job.find(params[:id])
+  end 
+
   def new
     @job = Job.new
-  end 
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    @job.save
+
+      redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
 end
